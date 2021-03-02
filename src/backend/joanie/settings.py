952e7d16d9b7abd64d5592547879823390e1d183
jbpenrath <jb.penrath@gmail.com>
2021-03-02 10:58:36 +0100

    ✅(pytest) use test environnment locally
    Django configuration environment was set to Test only on CI
    which create differences between tests run locally and from CI

--- src/backend/joanie/settings.py
+++ src/backend/joanie/settings.py
@@ -270,7 +270,7 @@ class Test(Base):
     """Test environment settings"""
 
     CACHES = {
-        "default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"},
+        "default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"},
     }
 
 
