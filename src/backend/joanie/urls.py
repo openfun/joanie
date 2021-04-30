cee2346389a84861f865ec23d19a362edcdb49fe
Samuel Paccoud - France Université Numérique <samuel.paccoud@fun-mooc.fr>
2021-04-30 23:08:26 +0200

    ♻️(api) move API definition files to joanie core app
    The API code is related to an app so its place is in the related
    app.

--- src/backend/joanie/urls.py
+++ src/backend/joanie/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from . import api
+from joanie.core import api
 
 urlpatterns = [
     path("admin/", admin.site.urls),
