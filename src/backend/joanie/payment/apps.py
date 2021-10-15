1d6cb581fe51c546da53c22af5a178fa3c045a89
jbpenrath <jb.penrath@gmail.com>
2021-10-15 18:21:03 +0200

    ➕(payplug) add payplug dependency
    Install payplug-python to interact with Payplug API.

--- src/backend/joanie/payment/apps.py
+++ src/backend/joanie/payment/apps.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class PaymentConfig(AppConfig):
-    """ Configuration class for the joanie payment app."""
+    """Configuration class for the joanie payment app."""
 
     name = "joanie.payment"
     verbose_name = _("Joanie payment application")
