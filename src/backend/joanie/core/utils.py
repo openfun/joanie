d9ae7478c3e57aefd7c632716d15e24425ebb666
jbpenrath <jb.penrath@gmail.com>
2021-12-10 11:22:31 +0100

    ✨(utils) add merge_dict util
    Add a merge_dict util to core app to simply deep merge two dicts

--- src/backend/joanie/core/utils.py
+++ src/backend/joanie/core/utils.py
@@ -2,6 +2,7 @@
 Utils that can be useful throughout Joanie's core app
 """
 import base64
+import collections.abc
 
 from django.utils.text import slugify
 
@@ -11,6 +12,23 @@ from PIL import ImageFile as PillowImageFile
 def normalize_code(code):
     """Normalize object codes to avoid duplicates."""
     return slugify(code, allow_unicode=False).upper() if code else None
+
+
+def merge_dict(base_dict, update_dict):
+    """Utility for deep dictionary updates.
+
+    >>> d1 = {'k1':{'k11':{'a': 0, 'b': 1}}}
+    >>> d2 = {'k1':{'k11':{'b': 10}, 'k12':{'a': 3}}}
+    >>> merge_dict(d1, d2)
+    {'k1': {'k11': {'a': 0, 'b': 10}, 'k12':{'a': 3}}}
+
+    """
+    for key, value in update_dict.items():
+        if isinstance(value, collections.abc.Mapping):
+            base_dict[key] = merge_dict(base_dict.get(key, {}), value)
+        else:
+            base_dict[key] = value
+    return base_dict
 
 
 def image_to_base64(file_or_path, close=False):
