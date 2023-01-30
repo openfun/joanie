638c51f1f4a7687a3af38062f636e42d8240d53e
Arthur Vuillard <arthur@hashbang.fr>
2023-01-30 11:35:04 +0100

    admin for more models

--- src/backend/joanie/jenny/admin.py
+++ src/backend/joanie/jenny/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 
-from .models import Pricing, Product, ProductPrice, ProductPriceRange, ProductPricePackLine
+from .models import Pricing, Product, ProductPrice, ProductPriceRange, ProductPricePackLine, Transaction, Quote, Invoice, CourseSubmission, Contract, InvoiceLine, QuoteLine
 
+class InvoiceLineInline(admin.TabularInline):
+    model = InvoiceLine
+    extra = 0
+class InvoiceAdmin(admin.ModelAdmin):
+    inlines = [InvoiceLineInline]
+
+
+class QuoteLineInline(admin.TabularInline):
+    model = QuoteLine
+    extra = 0
+
+
+class QuoteAdmin(admin.ModelAdmin):
+    inlines = [QuoteLineInline]
 
 class ProductPriceRangeInline(admin.TabularInline):
     model = ProductPriceRange
@@ -35,3 +49,8 @@ admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductPrice, ProductPriceAdmin)
 admin.site.register(ProductPriceRange)
 admin.site.register(ProductPricePackLine)
+admin.site.register(Transaction)
+admin.site.register(Contract)
+admin.site.register(Invoice, InvoiceAdmin)
+admin.site.register(Quote, QuoteAdmin)
+admin.site.register(CourseSubmission)
