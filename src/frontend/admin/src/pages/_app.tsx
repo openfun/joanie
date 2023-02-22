d917f5216a57d84b68ff012e451f8f9ff2f8ab90
nathanpanchout <nathanpanchout@live.fr>
2023-02-22 14:49:41 +0100

    🌐(frontend) add react-intl stack
    Creation of a component to manage the initialization of language
    and change of languages. Adding frontend and backend translation files.

--- src/frontend/admin/src/pages/_app.tsx
+++ src/frontend/admin/src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import "@/styles/globals.scss";
-import { NextPage } from "next";
-import { AppProps } from "next/app";
-import { useEffect, useState } from "react";
+import type { AppProps } from "next/app";
 import { CacheProvider } from "@emotion/react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { NextPage } from "next";
 import createEmotionCache from "@/utils/createEmotionCache";
-import { TranslationsProvider } from "@/contexts/i18n/TranslationsProvider/TranslationsProvider";
+import { TranslationsProvider } from "@/components/i18n/TranslationsProvider/TranslationsProvider";
 import { LocalesEnum } from "@/types/i18n/LocalesEnum";
 
 type NextPageWithLayout = NextPage & {
@@ -18,26 +21,6 @@ interface MyAppProps extends AppProps {
 const clientSideEmotionCache = createEmotionCache();
 
 export default function App({ Component, pageProps }: MyAppProps) {
-  const [shouldRender, setShouldRender] = useState(
-    !(process.env.NEXT_PUBLIC_API_MOCKING === "enabled")
-  );
-
-  useEffect(() => {
-    async function initMsw() {
-      const { initMocks } = await import("../../mocks");
-      await initMocks();
-      setShouldRender(true);
-    }
-
-    if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
-      initMsw();
-    }
-  }, []);
-
-  if (!shouldRender) {
-    return null;
-  }
-
   return (
     <TranslationsProvider locale={LocalesEnum.FRENCH}>
       <CacheProvider value={clientSideEmotionCache}>
