00072fded08a1364dc93748b5e3faaed5c7e4433
nathanpanchout <nathanpanchout@live.fr>
2023-04-20 15:01:09 +0200

    ✨(frontend) add course pages
    Add course management pages, i.e. the list, the creation and
    edition page

---
+++ src/frontend/admin/src/pages/admin/courses/[id]/edit.tsx
@@ -0,0 +1,47 @@
+import { defineMessages, useIntl } from "react-intl";
+import { useRouter } from "next/router";
+import { DashboardLayoutPage } from "@/layouts/dashboard/page/DashboardLayoutPage";
+import { PATH_ADMIN } from "@/utils/routes/path";
+import { SimpleCard } from "@/components/presentational/card/SimpleCard";
+import { coursesPagesTranslation } from "@/translations/pages/courses/breadcrumbsTranslations";
+import { CourseForm } from "@/components/templates/courses/form/CourseForm";
+import { useCourse } from "@/hooks/useCourses/useCourses";
+
+const messages = defineMessages({
+  pageTitle: {
+    id: "pages.admin.coursesRuns.edit.pageTitle",
+    defaultMessage: "Edit course: {courseName}",
+    description: "Label for the edit course run page title",
+  },
+});
+
+export default function EditCourseRunPage() {
+  const intl = useIntl();
+  const { query } = useRouter();
+  const courseRun = useCourse(query.id as string);
+  return (
+    <DashboardLayoutPage
+      title={intl.formatMessage(messages.pageTitle, {
+        courseName: courseRun?.item?.title,
+      })}
+      breadcrumbs={[
+        {
+          name: intl.formatMessage(coursesPagesTranslation.rootBreadcrumb),
+        },
+        {
+          name: intl.formatMessage(coursesPagesTranslation.listBreadcrumb),
+          href: PATH_ADMIN.courses.list,
+        },
+        {
+          name: intl.formatMessage(coursesPagesTranslation.editBreadcrumb),
+          isActive: true,
+        },
+      ]}
+      stretch={false}
+    >
+      <SimpleCard>
+        {courseRun.item && <CourseForm course={courseRun.item} />}
+      </SimpleCard>
+    </DashboardLayoutPage>
+  );
+}
