76dfba3ae12e715a4d9831c60be9a50b418e0881
nathanpanchout <nathanpanchout@live.fr>
2023-04-20 14:58:45 +0200

    ✨(frontend) add organization pages
    Add organization management pages, i.e. the list, the creation and
    edition page

---
+++ src/frontend/admin/src/pages/admin/organizations/create.tsx
@@ -0,0 +1,46 @@
+import { defineMessages, useIntl } from "react-intl";
+import { useRouter } from "next/router";
+import { DashboardLayoutPage } from "@/layouts/dashboard/page/DashboardLayoutPage";
+import { OrganizationForm } from "@/components/templates/organizations/form/OrganizationForm";
+import { SimpleCard } from "@/components/presentational/card/SimpleCard";
+import { PATH_ADMIN } from "@/utils/routes/path";
+import { orgBreadcrumbsTranslation } from "@/translations/pages/organizations/breadcrumbsTranslations";
+
+const messages = defineMessages({
+  pageTitle: {
+    id: "pages.admin.organizations.create.pageTitle",
+    defaultMessage: "Add organization",
+    description: "Label for the create organization page title",
+  },
+});
+
+export default function CreateOrganizationPage() {
+  const intl = useIntl();
+  const router = useRouter();
+  return (
+    <DashboardLayoutPage
+      title={intl.formatMessage(messages.pageTitle)}
+      breadcrumbs={[
+        {
+          name: intl.formatMessage(orgBreadcrumbsTranslation.rootBreadcrumb),
+        },
+        {
+          name: intl.formatMessage(orgBreadcrumbsTranslation.listBreadcrumb),
+          href: PATH_ADMIN.organizations.list,
+        },
+        {
+          name: intl.formatMessage(orgBreadcrumbsTranslation.createBreadcrumb),
+          isActive: true,
+        },
+      ]}
+    >
+      <SimpleCard>
+        <OrganizationForm
+          afterSubmit={(organization) =>
+            router.push(PATH_ADMIN.organizations.edit(organization.id))
+          }
+        />
+      </SimpleCard>
+    </DashboardLayoutPage>
+  );
+}
--- src/frontend/admin/src/pages/courses-runs/index.tsx
+++
@@ -1,5 +0,0 @@
-import { DashboardLayoutPage } from "@/layouts/dashboard/page/DashboardLayoutPage";
-
-export default function CoursesRuns() {
-  return <DashboardLayoutPage>CoursesRuns pages</DashboardLayoutPage>;
-}
