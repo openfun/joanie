e954ac5295c8592f5bfec0c57955032aadd9cee6
nathanpanchout <nathanpanchout@live.fr>
2023-04-20 15:06:39 +0200

    🔧(frontend) update jest configuration
    The tests were no longer working because of some libs. So we update the jest
    configuration

--- src/frontend/admin/jest.setup.js
+++ src/frontend/admin/jest.setup.js
@@ -1,12 +1,19 @@
 import "@testing-library/jest-dom/extend-expect";
 import "whatwg-fetch";
 import { server } from "./mocks/server";
+import { QueryCache } from "@tanstack/react-query";
 
-module.exports = async () => {
-  process.env.TZ = "UTC";
-};
+const queryCache = new QueryCache();
+
+jest.mock("next/router", () => require("next-router-mock"));
 
 // Establish API mocking before all tests.
+
+beforeEach(() => {
+  const location = new URL("http://localhost:3000/");
+  delete window.location;
+  window.location = location;
+});
 beforeAll(() => {
   server.listen();
 });
@@ -17,4 +24,7 @@ afterEach(() => {
   server.resetHandlers();
 });
 // Clean up after the tests are finished.
-afterAll(() => server.close());
+afterAll(() => {
+  server.close();
+  queryCache.clear();
+});
