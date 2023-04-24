49cb16c69b83c55e76cc0f48edd1de8bb971b3a7
jbpenrath <jb.penrath@gmail.com>
2023-04-24 15:12:39 +0200

    ✨(core) create a serializer ThumbnailDetailField
    We want to be able to serializer model ThumbnailImageField properly to allow
    frontend consumer to setup easily responsive image tags. So, we create a
    ThumbnailDetailField which inherits of ImageDetailField then build src and
    srcset attributes through available thumbnails information.

--- src/backend/joanie/core/serializers/fields.py
+++ src/backend/joanie/core/serializers/fields.py
@@ -1,20 +1,65 @@
 """Custom serializer fields."""
+from easy_thumbnails.alias import aliases
+from easy_thumbnails.files import ThumbnailerImageFieldFile
 from rest_framework import serializers
 
 
 class ImageDetailField(serializers.ImageField):
     """Custom serializer ImageField to return image details as dict."""
 
+    def get_absolute_url(self, value):
+        """Build absolute url from value."""
+        try:
+            url = value.url
+        except AttributeError:
+            return None
+        request = self.context.get("request", None)
+        if request is not None:
+            return request.build_absolute_uri(url)
+
+        return url
+
     def to_representation(self, value):
         """Return image filename, url and dimensions."""
-        if value:
-            # ImageField returns the file url containing the hostname, we want to keep
-            # this behavior.
-            url_with_hostname = super().to_representation(value)
-            return {
-                "filename": value.name,
-                "url": url_with_hostname,
-                "height": value.height,
-                "width": value.width,
-            }
-        return None
+        if not value:
+            return None
+
+        return {
+            "filename": value.name,
+            "height": value.height,
+            "width": value.width,
+            "src": self.get_absolute_url(value),
+            "size": value.size,
+        }
+
+
+class ThumbnailDetailField(ImageDetailField):
+    """
+    Custom serializer field for ThumbnailImageField to return thumbnail details as dict.
+    """
+
+    def to_representation(self, value):
+        """Return thumbnail filename, src, srcset and dimensions."""
+        representation = super().to_representation(value)
+        is_thumbnail = isinstance(value, ThumbnailerImageFieldFile)
+
+        if not representation:
+            return None
+
+        if is_thumbnail:
+            # - src
+            representation["src"] = self.get_absolute_url(
+                value.get_thumbnail({"size": (value.width, value.height)})
+            )
+
+            # - srcset
+            all_options = aliases.all(value, include_global=False) or aliases.all(value)
+            if all_options:
+                srcset = []
+                for descriptor, options in all_options.items():
+                    thumbnail = value.get_thumbnail(options)
+                    url = self.get_absolute_url(thumbnail)
+                    srcset.append(f"{url} {descriptor}")
+                representation["srcset"] = ", ".join(srcset)
+
+        return representation
