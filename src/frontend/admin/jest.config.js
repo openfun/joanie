775d5da1d731432ea99f78973defc15bc6a2e554
nathanpanchout <nathanpanchout@live.fr>
2023-05-23 15:49:55 +0200

    💚(frontend) fix ci test-front-admin
    In the IC, the tests of the fronts no longer pass.
    It lacks environment variables and options for the tests to pass correctly.

--- src/frontend/admin/jest.config.js
+++ src/frontend/admin/jest.config.js
@@ -2,6 +2,7 @@
 /** @type {import('jest').Config} */
 const customJestConfig = {
   // Add more setup options before each test is run
+  setupFiles: ["<rootDir>/jest.env.js"],
   setupFilesAfterEnv: ["@testing-library/jest-dom", "<rootDir>/jest.setup.js"],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
