ccfdfcdafd515de34c6ba1737a8013a336069639
jbpenrath <jb.penrath@gmail.com>
2023-07-05 22:28:09 +0200

    ♻️(core) remove AdminViewSet
    Previously we created a generic AdminViewSet class which was applied to all
    admin Viewsets. This class was in charge to enable full crud operations, add
    specific permissions for admin and define authentication classes. But now, we do
     not want to enable crud operations for all viewsets to we decide to remove this
      generic Viewset and instead just factorize the admin permissions.

--- src/backend/joanie/core/api/admin.py
+++ src/backend/joanie/core/api/admin.py
@@ -8,61 +8,71 @@ from joanie.core import filters, models, serializers
 
 
 # pylint: disable=too-many-ancestors
-class AdminViewSet(viewsets.ModelViewSet):
-    """Admin ViewSet used as base by all admin ViewSets to restrict to admin users only."""
+class HasAdminPermissions(permissions.IsAdminUser, permissions.DjangoModelPermissions):
+    """
+    Default permissions for admin endpoints. It ensures that the user is an admin and
+    has the required permissions for the model.
+    """
 
-    authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.IsAdminUser, permissions.DjangoModelPermissions]
 
-
-class OrganizationViewSet(AdminViewSet):
+class OrganizationViewSet(viewsets.ModelViewSet):
     """
     Admin Organization ViewSet
     """
 
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [HasAdminPermissions]
     serializer_class = serializers.AdminOrganizationSerializer
     queryset = models.Organization.objects.all()
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
     filterset_class = filters.OrganizationAdminFilterSet
 
 
-class ProductViewSet(AdminViewSet):
+class ProductViewSet(viewsets.ModelViewSet):
     """
     Admin Product ViewSet
     """
 
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [HasAdminPermissions]
     serializer_class = serializers.AdminProductSerializer
     queryset = models.Product.objects.all()
     filterset_class = filters.ProductAdminFilterSet
 
 
-class CourseViewSet(AdminViewSet):
+class CourseViewSet(viewsets.ModelViewSet):
     """
     Admin Course ViewSet
     """
 
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [HasAdminPermissions]
     serializer_class = serializers.AdminCourseSerializer
     queryset = models.Course.objects.all().prefetch_related("organizations", "products")
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
     filterset_class = filters.CourseAdminFilterSet
 
 
-class CourseRunViewSet(AdminViewSet):
+class CourseRunViewSet(viewsets.ModelViewSet):
     """
     Admin CourseRun ViewSet
     """
 
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [HasAdminPermissions]
     serializer_class = serializers.AdminCourseRunSerializer
     queryset = models.CourseRun.objects.all().select_related("course")
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
     filterset_class = filters.CourseRunAdminFilterSet
 
 
-class CertificateDefinitionViewSet(AdminViewSet):
+class CertificateDefinitionViewSet(viewsets.ModelViewSet):
     """
     Admin Certificate ViewSet
     """
 
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [HasAdminPermissions]
     serializer_class = serializers.AdminCertificateDefinitionSerializer
     queryset = models.CertificateDefinition.objects.all()
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
