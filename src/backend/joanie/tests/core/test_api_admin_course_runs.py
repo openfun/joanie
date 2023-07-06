73a8dd07422fed3d6f69b305b454c48fe6aa7734
Samy Ait-Ouakli <samy.aitouakli@polyconseil.fr>
2023-07-06 18:18:03 +0200

    ✨(backend) add filters to CourseRun list
    Add filters (name, start_date, state) to the list of course run
    of a given course

--- src/backend/joanie/tests/core/test_api_admin_course_runs.py
+++ src/backend/joanie/tests/core/test_api_admin_course_runs.py
@@ -2,10 +2,11 @@
 Test suite for Course run Admin API.
 """
 import random
+from datetime import datetime, timedelta
 
 from django.test import TestCase
 
-from joanie.core import factories
+from joanie.core import factories, models
 
 
 class CourseRunAdminApiTest(TestCase):
@@ -233,3 +234,129 @@ class CourseRunAdminApiTest(TestCase):
         response = self.client.delete(f"/api/v1.0/admin/course-runs/{course_run.id}/")
 
         self.assertEqual(response.status_code, 204)
+
+    def test_admin_api_course_run_read_list_authenticated_with_nested_course_filter_name(
+        self,
+    ):
+        """
+        When filtering by name the query returns only relevant CourseRuns
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        course = factories.CourseFactory.create()
+        course_run = factories.CourseRunFactory(
+            course=course, is_listed=True, title="This is a test"
+        )
+        factories.CourseRunFactory(course=course, is_listed=True, title="CourseRun 0")
+        factories.CourseRunFactory(course=course, is_listed=True, title="CourseRun 1")
+        factories.CourseRunFactory(course=course, is_listed=True, title="CourseRun 2")
+
+        response = self.client.get(
+            f"/api/v1.0/admin/courses/{course.id}/course-runs/?query=is%20a",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        content = response.json()
+        self.assertEqual(content["count"], 1)
+        self.assertEqual(content["results"][0]["id"], str(course_run.id))
+        response = self.client.get(
+            f"/api/v1.0/admin/courses/{course.id}/course-runs/?query=Cour",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        content = response.json()
+        self.assertEqual(content["count"], 3)
+
+    def test_admin_api_course_run_read_list_authenticated_with_nested_course_filter_start_date(
+        self,
+    ):
+        """
+        When filtering by date the query returns only CourseRuns starting at
+        or after the given date
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        now = datetime.utcnow()
+        course = factories.CourseFactory.create()
+        course_run = factories.CourseRunFactory(
+            course=course, is_listed=True, start=now
+        )
+        factories.CourseRunFactory(
+            course=course, is_listed=True, start=now - timedelta(weeks=20)
+        )
+
+        response = self.client.get(
+            f"/api/v1.0/admin/courses/{course.id}/course-runs/?start={now.isoformat()}",
+        )
+        self.assertEqual(response.status_code, 200)
+        content = response.json()
+        self.assertEqual(content["count"], 1)
+        self.assertEqual(content["results"][0]["id"], str(course_run.id))
+
+        past_date = now - timedelta(weeks=30)
+        response = self.client.get(
+            f"/api/v1.0/admin/courses/{course.id}/course-runs/?start={past_date.isoformat()}",
+        )
+        self.assertEqual(response.status_code, 200)
+        content = response.json()
+        self.assertEqual(content["count"], 2)
+
+        future_date = now + timedelta(weeks=30)
+        response = self.client.get(
+            f"/api/v1.0/admin/courses/{course.id}/course-runs/?start={future_date.isoformat()}",
+        )
+        self.assertEqual(response.status_code, 200)
+        content = response.json()
+        self.assertEqual(content["count"], 0)
+
+    def test_admin_api_course_run_read_list_authenticated_with_nested_course_filter_state(
+        self,
+    ):
+        """
+        When filtering by state, the query returns only CourseRuns matching the given state.
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        now = datetime.utcnow()
+        course = factories.CourseFactory.create()
+        date_pairs = [
+            (now - timedelta(hours=3), now - timedelta(hours=3)),
+            (now - timedelta(hours=3), now + timedelta(hours=3)),
+            (now + timedelta(hours=3), now + timedelta(hours=3)),
+        ]
+        for start, end in date_pairs:
+            for enrollment_start, enrollment_end in date_pairs:
+                factories.CourseRunFactory(
+                    course=course,
+                    start=start,
+                    end=end,
+                    enrollment_start=enrollment_start,
+                    enrollment_end=enrollment_end,
+                )
+        factories.CourseRunFactory.create_batch(20, course=course)
+        all_course_runs = models.CourseRun.objects.all()
+        sorted_course_runs = [[] for _ in range(len(models.CourseState.STATE_TEXTS))]
+
+        for course_run in all_course_runs:
+            sorted_course_runs[course_run.state["priority"]].append(course_run)
+
+        for state_id in models.CourseState.STATE_TEXTS:
+            response = self.client.get(
+                (
+                    f"/api/v1.0/admin/courses/{course.id}/course-runs/"
+                    f"?state={state_id}"
+                ),
+            )
+            self.assertEqual(response.status_code, 200)
+            content = response.json()
+            self.assertEqual(content["count"], len(sorted_course_runs[state_id]))
+            for course_run in sorted_course_runs[state_id]:
+                self.assertTrue(str(course_run.id) in str(content))
+
+        # Non existing state
+        response = self.client.get(
+            f"/api/v1.0/admin/courses/{course.id}/course-runs/?state=-1",
+        )
+        self.assertEqual(response.status_code, 200)
+        content = response.json()
+        self.assertEqual(content["count"], 0)
