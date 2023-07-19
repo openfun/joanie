34816a96023d0bccad1491a825941a2e1b20c887
jbpenrath <jb.penrath@gmail.com>
2023-07-19 18:24:27 +0200

    ✨(api/admin) add endpoint to create/udpate/delete course accesses
    Backoffice needs to create / update / delete course accesses so we create api
    endpoints to allow that.

---
+++ src/backend/joanie/tests/core/test_api_admin_course_access.py
@@ -0,0 +1,329 @@
+"""
+Test suite for CourseAccess Admin API.
+"""
+import uuid
+
+from django.test import TestCase
+
+from joanie.core import enums, factories
+
+
+class CourseAccessAdminApiTest(TestCase):
+    """
+    Test suite for CourseAccess Admin API.
+    """
+
+    def test_admin_api_course_accesses_request_anonymous(self):
+        """
+        Anonymous users should not be able to request course accesses endpoint.
+        """
+        course = factories.CourseFactory()
+        response = self.client.get(f"/api/v1.0/admin/courses/{course.id}/accesses/")
+
+        self.assertEqual(response.status_code, 403)
+        content = response.json()
+        self.assertEqual(
+            content["detail"], "Authentication credentials were not provided."
+        )
+
+    def test_admin_api_course_accesses_request_authenticated(self):
+        """
+        Authenticated users should not be able to request course accesses endpoint.
+        """
+        user = factories.UserFactory(is_staff=False, is_superuser=False)
+        self.client.login(username=user.username, password="password")
+        course = factories.CourseFactory()
+        response = self.client.get(f"/api/v1.0/admin/courses/{course.id}/accesses/")
+
+        self.assertContains(
+            response,
+            "You do not have permission to perform this action.",
+            status_code=403,
+        )
+
+    def test_admin_api_course_accesses_request_list(self):
+        """
+        Super admin user should not be able to list course accesses.
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        course = factories.CourseFactory()
+        response = self.client.get(f"/api/v1.0/admin/courses/{course.id}/accesses/")
+
+        self.assertContains(
+            response,
+            'Method \\"GET\\" not allowed.',
+            status_code=405,
+        )
+
+    def test_admin_api_course_accesses_request_get(self):
+        """
+        Super admin user should not be able to retrieve a course accesses.
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        course = factories.CourseFactory()
+        course_access = factories.UserCourseAccessFactory(course=course)
+        response = self.client.get(
+            f"/api/v1.0/admin/courses/{course.id}/accesses/{course_access.id}/"
+        )
+
+        self.assertContains(
+            response,
+            'Method \\"GET\\" not allowed.',
+            status_code=405,
+        )
+
+    def test_admin_api_course_accesses_request_create(self):
+        """
+        Super admin user should be able to create a course access.
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        course = factories.CourseFactory()
+        user = factories.UserFactory()
+        self.assertEqual(course.accesses.count(), 0)
+
+        response = self.client.post(
+            f"/api/v1.0/admin/courses/{course.id}/accesses/",
+            data={
+                "user": str(user.id),
+                "role": enums.MANAGER,
+            },
+        )
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(course.accesses.count(), 1)
+        course_access = course.accesses.first()
+        content = response.json()
+        self.assertEqual(
+            content,
+            {
+                "id": str(course_access.id),
+                "role": "manager",
+                "user": {
+                    "id": str(user.id),
+                    "username": user.username,
+                    "full_name": user.get_full_name(),
+                },
+            },
+        )
+
+    def test_admin_api_course_accesses_request_create_with_unknown_user_id(self):
+        """
+        An 400 Bad request error should be raised if the user id is unknown.
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        course = factories.CourseFactory()
+        user = factories.UserFactory.build()
+        self.assertEqual(course.accesses.count(), 0)
+
+        response = self.client.post(
+            f"/api/v1.0/admin/courses/{course.id}/accesses/",
+            data={
+                "user": str(user.id),
+                "role": enums.MANAGER,
+            },
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"user": ["Resource does not exist."]})
+
+    def test_admin_api_course_accesses_request_create_with_unknown_course_id(self):
+        """
+        An 404 Not found error should be raised if the course id is unknown.
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        course = factories.CourseFactory.build()
+        user = factories.UserFactory()
+        self.assertEqual(course.accesses.count(), 0)
+
+        response = self.client.post(
+            f"/api/v1.0/admin/courses/{course.id}/accesses/",
+            data={
+                "user": str(user.id),
+                "role": enums.MANAGER,
+            },
+        )
+
+        self.assertContains(response, "Not found.", status_code=404)
+
+    def test_admin_api_course_accesses_request_create_with_invalid_role(self):
+        """
+        An 400 Bad request error should be raised if the role is not valid.
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        course = factories.CourseFactory()
+        user = factories.UserFactory()
+        self.assertEqual(course.accesses.count(), 0)
+
+        response = self.client.post(
+            f"/api/v1.0/admin/courses/{course.id}/accesses/",
+            data={
+                "user": str(user.id),
+                "role": "invalid_role",
+            },
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response.json(), {"role": ['"invalid_role" is not a valid choice.']}
+        )
+
+    def test_admin_api_course_accesses_request_update(self):
+        """
+        Super admin user should be able to update a course access.
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        course = factories.CourseFactory()
+        user = factories.UserFactory()
+        course_access = factories.UserCourseAccessFactory(
+            course=course, user=user, role=enums.MANAGER
+        )
+
+        response = self.client.put(
+            f"/api/v1.0/admin/courses/{course.id}/accesses/{course_access.id}/",
+            content_type="application/json",
+            data={
+                "user": str(user.id),
+                "role": enums.INSTRUCTOR,
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        content = response.json()
+        self.assertEqual(
+            content,
+            {
+                "id": str(course_access.id),
+                "role": "instructor",
+                "user": {
+                    "id": str(user.id),
+                    "username": user.username,
+                    "full_name": user.get_full_name(),
+                },
+            },
+        )
+
+    def test_admin_api_course_accesses_request_update_with_unknown_course_id(self):
+        """
+        An 404 Not found error should be raised if the course access id is unknown.
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        course = factories.CourseFactory.build()
+        user = factories.UserFactory()
+        self.assertEqual(course.accesses.count(), 0)
+
+        response = self.client.put(
+            f"/api/v1.0/admin/courses/{course.id}/accesses/{uuid.uuid4()}/",
+            data={
+                "user": str(user.id),
+                "role": enums.MANAGER,
+            },
+        )
+
+        self.assertContains(response, "Not found.", status_code=404)
+
+    def test_admin_api_course_accesses_request_update_with_partial_payload(self):
+        """
+        An 400 Bad request error should be raised if a partial payload is provided.
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        course = factories.CourseFactory()
+        user = factories.UserFactory()
+        course_access = factories.UserCourseAccessFactory(
+            course=course, user=user, role=enums.MANAGER
+        )
+
+        response = self.client.put(
+            f"/api/v1.0/admin/courses/{course.id}/accesses/{course_access.id}/",
+            content_type="application/json",
+            data={
+                "role": enums.INSTRUCTOR,
+            },
+        )
+
+        self.assertEqual(response.status_code, 400)
+        content = response.json()
+        self.assertEqual(content, {"user": ["This field is required."]})
+
+    def test_admin_api_course_accesses_request_update_with_unknown_user(self):
+        """
+        An 400 Bad request error should be raised if the user id is unknown.
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        course = factories.CourseFactory()
+        user = factories.UserFactory.build()
+        self.assertEqual(course.accesses.count(), 0)
+
+        response = self.client.post(
+            f"/api/v1.0/admin/courses/{course.id}/accesses/",
+            data={
+                "user": str(user.id),
+                "role": enums.MANAGER,
+            },
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"user": ["Resource does not exist."]})
+
+    def test_admin_api_course_accesses_request_partial_update(self):
+        """
+        Super admin user should be able to partially update a course access.
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        course = factories.CourseFactory()
+        user = factories.UserFactory()
+        course_access = factories.UserCourseAccessFactory(
+            course=course, user=user, role=enums.MANAGER
+        )
+
+        response = self.client.patch(
+            f"/api/v1.0/admin/courses/{course.id}/accesses/{course_access.id}/",
+            content_type="application/json",
+            data={
+                "role": enums.INSTRUCTOR,
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        content = response.json()
+        self.assertEqual(
+            content,
+            {
+                "id": str(course_access.id),
+                "role": "instructor",
+                "user": {
+                    "id": str(user.id),
+                    "username": user.username,
+                    "full_name": user.get_full_name(),
+                },
+            },
+        )
+
+    def test_admin_api_course_accesses_request_delete(self):
+        """
+        Super admin user should be able to delete a course access.
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+        course = factories.CourseFactory()
+        user = factories.UserFactory()
+        course_access = factories.UserCourseAccessFactory(
+            course=course, user=user, role=enums.MANAGER
+        )
+
+        response = self.client.delete(
+            f"/api/v1.0/admin/courses/{course.id}/accesses/{course_access.id}/"
+        )
+
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(course.accesses.count(), 0)
