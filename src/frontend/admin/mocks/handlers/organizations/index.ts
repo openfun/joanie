33b88bb789483f0f65614d717cfc5d4f0de5ab87
nathanpanchout <nathanpanchout@live.fr>
2023-07-21 10:49:41 +0200

    ✨(frontend) update msw handlers
    We need to modify the handlers to integrate the accesses into the request mock

--- src/frontend/admin/mocks/handlers/organizations/index.ts
+++ src/frontend/admin/mocks/handlers/organizations/index.ts
@@ -11,4 +11,41 @@ export const organizationHandlers = [
   rest.get(buildApiUrl(organizationRoute.get(":id")), (req, res, ctx) => {
     return res(ctx.json(OrganizationFactory()));
   }),
+  rest.post(
+    buildApiUrl(organizationRoute.addUserAccess(":id")),
+    (req, res, ctx) => {
+      return res(ctx.json(OrganizationFactory()));
+    },
+  ),
+  rest.options(buildApiUrl(organizationRoute.options), (req, res, ctx) => {
+    const result = {
+      actions: {
+        POST: {
+          accesses: {
+            child: {
+              children: {
+                role: {
+                  choices: [
+                    {
+                      value: "owner",
+                      display_name: "owner",
+                    },
+                    {
+                      value: "administrator",
+                      display_name: "administrator",
+                    },
+                    {
+                      value: "member",
+                      display_name: "member",
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+    return res(ctx.json(result));
+  }),
 ];
