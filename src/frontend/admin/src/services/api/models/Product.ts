7e20247fe6ff9900d22282ac3af39914a67907b6
nathanpanchout <nathanpanchout@live.fr>
2023-09-11 13:29:33 +0200

    🏷️(frontend) add and update types
    With this feature, several types are impacted and factories. So we update these
    types

--- src/frontend/admin/src/services/api/models/Product.ts
+++ src/frontend/admin/src/services/api/models/Product.ts
@@ -1,6 +1,11 @@
 import type { CertificateDefinition } from "./CertificateDefinition";
-import { ProductTargetCourseRelation } from "./ProductTargetCourseRelation";
+import {
+  DTOProductTargetCourseRelation,
+  ProductTargetCourseRelation,
+} from "./ProductTargetCourseRelation";
 import { ProductRelationToCourse } from "./Relations";
+import { Nullable, Optional, ToFormValues } from "@/types/utils";
+import { ProductFormMainValues } from "@/components/templates/products/form/sections/main/ProductFormMain";
 
 export type Product = {
   id: string;
@@ -11,7 +16,7 @@ export type Product = {
   target_courses?: ProductTargetCourseRelation[];
   price?: number;
   price_currency?: string;
-  certificate_definitions?: CertificateDefinition;
+  certificate_definition?: CertificateDefinition;
   courses?: ProductRelationToCourse[];
 };
 
@@ -20,3 +25,50 @@ export enum ProductType {
   ENROLLMENT = "enrollment",
   CERTIFICATE = "certificate",
 }
+
+export type ProductFormValues = ToFormValues<{
+  type: ProductType;
+  title: string;
+  description: string;
+  call_to_action: string;
+  target_courses?: ProductTargetCourseRelation[];
+  price?: number;
+  price_currency?: string;
+  certificate_definition?: Nullable<CertificateDefinition>;
+}>;
+
+export type DTOProduct = {
+  id?: string;
+  type: ProductType;
+  title: string;
+  description?: string;
+  call_to_action: string;
+  price?: number;
+  price_currency?: string;
+  certificate_definition?: string;
+};
+
+export const transformProductToDTO = (
+  product: Product | ProductFormMainValues,
+): DTOProduct => {
+  return {
+    ...product,
+    certificate_definition: product.certificate_definition?.id,
+  };
+};
+
+export const transformProductTargetCourseRelationToDTO = (
+  target_course: Optional<ProductTargetCourseRelation, "id">,
+): DTOProductTargetCourseRelation => {
+  const courseRuns = target_course.course_runs?.map(
+    (course_run) => course_run.id,
+  );
+
+  return {
+    ...(target_course.id && { id: target_course.id }),
+    course: target_course.course?.id,
+    graded: target_course.graded ?? false,
+    position: target_course.position ?? 0,
+    course_runs: courseRuns ?? [],
+  };
+};
--- src/frontend/admin/src/services/factories/product/index.ts
+++ src/frontend/admin/src/services/factories/product/index.ts
@@ -1,6 +1,9 @@
 import { faker } from "@faker-js/faker";
 import { Product, ProductType } from "@/services/api/models/Product";
 import { CertificateDefinitionFactory } from "@/services/factories/certificate-definition";
+import { ProductRelationToCourseFactory } from "@/services/api/models/Relations";
+import { randomNumber } from "@/utils/numbers";
+import { ProductTargetCourseRelationFactory } from "@/services/api/models/ProductTargetCourseRelation";
 
 const build = (): Product => {
   return {
@@ -9,9 +12,11 @@ const build = (): Product => {
     type: ProductType.CREDENTIAL,
     description: faker.lorem.lines(2),
     call_to_action: "Buy",
-    price: 999,
-    price_currency: "$",
-    certificate_definitions: CertificateDefinitionFactory(),
+    price: faker.number.int({ min: 1000000 }),
+    price_currency: "EUR",
+    certificate_definition: CertificateDefinitionFactory(),
+    courses: ProductRelationToCourseFactory(randomNumber(5)),
+    target_courses: ProductTargetCourseRelationFactory(randomNumber(2)),
   };
 };
 
