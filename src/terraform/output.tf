a525f46877113b041132bca1da4794f7e58d83c6
Manuel Raynaud <manu@raynaud.io>
2023-11-08 14:25:57 +0100

    ♻️(back) remove usage of deprecated storage settings
    The settings related to store (the default storage and default static
    storage) are deprecated. Now we have to use the STORAGES settings.

--- src/terraform/output.tf
+++ src/terraform/output.tf
@@ -1,3 +1,7 @@
 output "objectstorage_bucket_name" {
   value = openstack_objectstorage_container_v1.joanie_media_storage.name
 }
+
+output "contracts_objectstorage_bucket_name" {
+  value = openstack_objectstorage_container_v1.joanie_contracts_storage.name
+}
