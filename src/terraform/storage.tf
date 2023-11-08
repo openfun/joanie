5ca01f65fa926ef2a3734633b16337d999d00fd0
Manuel Raynaud <manu@raynaud.io>
2023-11-08 14:25:57 +0100

    ♻️(back) remove usage of deprecated storage settings
    The settings related to store (the default storage and default static
    storage) are deprecated. Now we have to use the STORAGES settings.

--- src/terraform/storage.tf
+++ src/terraform/storage.tf
@@ -20,3 +20,21 @@ resource "openstack_objectstorage_container_v1" "joanie_media_storage" {
   container_read = "${data.openstack_identity_auth_scope_v3.current.project_id}:${var.user_name}"
   container_write = "${data.openstack_identity_auth_scope_v3.current.project_id}:${var.user_name}"
 }
+
+resource "openstack_objectstorage_container_v1" "joanie_contracts_storage" {
+  name          = "tf-${terraform.workspace}-joanie-contracts-storage"
+  provider      = openstack.ovh
+
+  # all objects should be deleted from the container so that the container
+  # can be destroyed without error.
+  force_destroy = true
+
+  metadata    = {
+    workspace = terraform.workspace
+  }
+
+  # Bucket is in read only for anonymous users.
+  # https://docs.openstack.org/swift/latest/overview_acl.html
+  container_read = "${data.openstack_identity_auth_scope_v3.current.project_id}:${var.user_name}"
+  container_write = "${data.openstack_identity_auth_scope_v3.current.project_id}:${var.user_name}"
+}
