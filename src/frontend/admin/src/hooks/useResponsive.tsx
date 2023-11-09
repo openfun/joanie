801c204d3f9ab506d68c4196e184b4c975a5c01c
nathanpanchout <nathanpanchout@live.fr>
2023-11-09 17:21:20 +0100

    🐛(frontend) fix DashboardNav layout
    When running the tests, we noticed that the dashboardNav component added an
    overlay. Because of this, Playwright thought the items were hidden.

---
+++ src/frontend/admin/src/hooks/useResponsive.tsx
@@ -0,0 +1,37 @@
+import { Breakpoint, useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+export type Query = "up" | "down" | "between" | "only";
+export type Value = Breakpoint | number;
+
+export function useResponsive(
+  query: Query,
+  start?: Value,
+  end?: Value,
+): boolean {
+  const theme = useTheme();
+
+  const mediaUp = useMediaQuery(theme.breakpoints.up(start as Value));
+
+  const mediaDown = useMediaQuery(theme.breakpoints.down(start as Value));
+
+  const mediaBetween = useMediaQuery(
+    theme.breakpoints.between(start as Value, end as Value),
+  );
+
+  const mediaOnly = useMediaQuery(theme.breakpoints.only(start as Breakpoint));
+
+  if (query === "up") {
+    return mediaUp;
+  }
+
+  if (query === "down") {
+    return mediaDown;
+  }
+
+  if (query === "between") {
+    return mediaBetween;
+  }
+
+  return mediaOnly;
+}
