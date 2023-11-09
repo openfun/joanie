7030e3c2322ab5fd84c7a3ec7db0298cf9de2c3a
nathanpanchout <nathanpanchout@live.fr>
2023-11-09 17:19:02 +0100

    ✨(frontend) update for playwright operation
    For the tests to run correctly, some internal logic must be added. Management of
    languages, users or even requests in test mode

--- src/frontend/admin/src/contexts/auth/AuthProvider.tsx
+++ src/frontend/admin/src/contexts/auth/AuthProvider.tsx
@@ -1,9 +1,26 @@
+import * as process from "process";
 import * as React from "react";
 import { PropsWithChildren, useEffect, useMemo, useState } from "react";
 import { AuthContext, AuthContextInterface } from "./AuthContext";
 import { AuthenticatedUser } from "@/types/auth";
 import { Maybe } from "@/types/utils";
 import { AuthRepository } from "@/services/repositories/auth/AuthRepository";
+
+const testUser = {
+  abilities: {
+    delete: false,
+    get: true,
+    has_course_access: true,
+    has_organization_access: true,
+    patch: true,
+    put: true,
+  },
+  full_name: "",
+  id: "ad2be34d-ab38-407f-a5d1-33eadb592023",
+  is_staff: true,
+  is_superuser: true,
+  username: "admin",
+};
 
 export function AuthProvider({ children }: PropsWithChildren) {
   const [user, setUser] = useState<Maybe<AuthenticatedUser>>(undefined);
@@ -16,7 +33,11 @@ export function AuthProvider({ children }: PropsWithChildren) {
   }, [user]);
 
   useEffect(() => {
-    AuthRepository.me().then(setUser);
+    if (process.env.NEXT_PUBLIC_API_SOURCE === "test") {
+      setUser(testUser);
+    } else {
+      AuthRepository.me().then(setUser);
+    }
   }, []);
 
   if (!authContext) return null;
