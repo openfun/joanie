43fef849eabbf204742461261a27d42b07782102
Nicolas Clerc <kernicpanel@nclerc.fr>
2023-11-20 11:39:43 +0100

    🏷️(backend) fix schema for file download
    Certificate download schema was not reflecting pdf download.
    ContractViewset download schema was not reflecting pdf download.
    ContractDefinitionViewset preview_template schema was not reflecting pdf
    download.

--- src/backend/joanie/core/serializers/base.py
+++ src/backend/joanie/core/serializers/base.py
@@ -30,3 +30,22 @@ class CachedModelSerializer(serializers.ModelSerializer):
             cache.set(cache_key, representation, cache_ttl)
 
         return representation
+
+
+class ErrorResponseSerializer(serializers.Serializer):
+    """
+    Serializer used to format error responses.
+    """
+
+    details = serializers.CharField(required=True)
+
+    class Meta:
+        fields = ["details"]
+
+    def create(self, validated_data):
+        """Abstract method that should be implemented."""
+        raise NotImplementedError()
+
+    def update(self, instance, validated_data):
+        """Abstract method that should be implemented."""
+        raise NotImplementedError()
