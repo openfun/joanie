e9f29d9788f5f250db8d72d440b28cdce6302eef
nathanpanchout <nathanpanchout@live.fr>
2023-11-27 17:49:06 +0100

    ✨(frontend) update repositories
    In order to be able to perform queries on the order groups, we need to add
    repositories and useResource hook

---
+++ src/frontend/admin/src/hooks/useCourseProductRelation/useCourseProductRelation.ts
@@ -0,0 +1,175 @@
+import { defineMessages, useIntl } from "react-intl";
+import { useMutation } from "@tanstack/react-query";
+import {
+  QueryOptions,
+  ResourcesQuery,
+  useResource,
+  useResourcesCustom,
+  UseResourcesProps,
+} from "@/hooks/useResources";
+import { CourseProductRelationRepository } from "@/services/repositories/course-product-relation/CourseProductRelationRepository";
+import { CourseRelationToProduct } from "@/services/api/models/Relations";
+import { DTOOrderGroup } from "@/services/api/models/OrderGroup";
+
+const messages = defineMessages({
+  errorUpdate: {
+    id: "hooks.useCourseProductRelation.errorUpdate",
+    description:
+      "Error message shown to the user when course product relation update request fails.",
+    defaultMessage:
+      "An error occurred while updating the course product relation. Please retry later.",
+  },
+  errorUpdateOrderGroup: {
+    id: "hooks.useCourseProductRelation.errorUpdateOrderGroup",
+    description:
+      "Error message shown to the user when order group update request fails.",
+    defaultMessage:
+      "An error occurred while updating the order group. Please retry later.",
+  },
+  errorGet: {
+    id: "hooks.useCourseProductRelation.errorGet",
+    description:
+      "Error message shown to the user when course product relations fetch request fails.",
+    defaultMessage:
+      "An error occurred while fetching course product relations. Please retry later.",
+  },
+  errorDelete: {
+    id: "hooks.useCourseProductRelation.errorDelete",
+    description:
+      "Error message shown to the user when course product relation deletion request fails.",
+    defaultMessage:
+      "An error occurred while deleting the course product relation. Please retry later.",
+  },
+  errorDeleteOrderGroup: {
+    id: "hooks.useCourseProductRelation.errorDeleteOrderGroup",
+    description:
+      "Error message shown to the user when order group deletion request fails.",
+    defaultMessage:
+      "An error occurred while deleting the order group. Please retry later.",
+  },
+  errorCreate: {
+    id: "hooks.useCourseProductRelation.errorCreate",
+    description:
+      "Error message shown to the user when course product relation creation request fails.",
+    defaultMessage:
+      "An error occurred while creating the course product relation. Please retry later.",
+  },
+  errorCreateOrderGroup: {
+    id: "hooks.useCourseProductRelation.errorCreateOrderGroup",
+    description:
+      "Error message shown to the user when order group creation request fails.",
+    defaultMessage:
+      "An error occurred while creating the order group. Please retry later.",
+  },
+  errorNotFound: {
+    id: "hooks.useCourseProductRelation.errorNotFound",
+    description:
+      "Error message shown to the user when no course product relations matches.",
+    defaultMessage: "Cannot find the course product relation",
+  },
+});
+
+/**
+ * Joanie Api hook to retrieve/create/update/delete course product relations
+ * owned by the authenticated user.
+ */
+const props: UseResourcesProps<CourseRelationToProduct, ResourcesQuery> = {
+  queryKey: ["course-product-relation"],
+  apiInterface: () => ({
+    get: async (filters) => {
+      if (filters?.id) {
+        const { id, ...otherFilters } = filters;
+        return CourseProductRelationRepository.get(id, otherFilters);
+      }
+
+      return CourseProductRelationRepository.getAll(filters);
+    },
+    create: CourseProductRelationRepository.create,
+    update: async ({ id, ...payload }) => {
+      return CourseProductRelationRepository.update(id, payload);
+    },
+    delete: async (id: string) => {
+      return CourseProductRelationRepository.delete(id);
+    },
+  }),
+  session: true,
+  messages,
+};
+
+export const useCourseProductRelations = (
+  filters?: ResourcesQuery,
+  queryOptions?: QueryOptions<CourseRelationToProduct>,
+) => {
+  const intl = useIntl();
+  const custom = useResourcesCustom({ ...props, filters, queryOptions });
+  const mutation = useMutation;
+
+  return {
+    ...custom,
+    methods: {
+      ...custom.methods,
+      addOrderGroup: mutation({
+        mutationFn: async (data: {
+          relationId: string;
+          payload: DTOOrderGroup;
+        }) => {
+          return CourseProductRelationRepository.addOrderGroup(
+            data.relationId,
+            data.payload,
+          );
+        },
+        onSuccess: () => {
+          custom.methods.invalidate();
+        },
+        onError: () => {
+          custom.methods.setError(
+            intl.formatMessage(messages.errorCreateOrderGroup),
+          );
+        },
+      }).mutate,
+      editOrderGroup: mutation({
+        mutationFn: async (data: {
+          relationId: string;
+          orderGroupId: string;
+          payload: DTOOrderGroup;
+        }) => {
+          return CourseProductRelationRepository.editOrderGroup(
+            data.relationId,
+            data.orderGroupId,
+            data.payload,
+          );
+        },
+        onSuccess: () => {
+          custom.methods.invalidate();
+        },
+        onError: () => {
+          custom.methods.setError(
+            intl.formatMessage(messages.errorUpdateOrderGroup),
+          );
+        },
+      }).mutate,
+      deleteOrderGroup: mutation({
+        mutationFn: async (data: {
+          relationId: string;
+          orderGroupId: string;
+        }) => {
+          return CourseProductRelationRepository.deleteOrderGroup(
+            data.relationId,
+            data.orderGroupId,
+          );
+        },
+        onSuccess: () => {
+          custom.methods.invalidate();
+        },
+        onError: () => {
+          custom.methods.setError(
+            intl.formatMessage(messages.errorDeleteOrderGroup),
+          );
+        },
+      }).mutate,
+    },
+  };
+};
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+export const useCourseProductRelation = useResource(props);
---
+++ src/frontend/admin/src/services/repositories/course-product-relation/CourseProductRelationRepository.ts
@@ -0,0 +1,118 @@
+import queryString from "query-string";
+import { ResourcesQuery } from "@/hooks/useResources";
+import { Maybe } from "@/types/utils";
+import {
+  checkStatus,
+  fetchApi,
+  getAcceptLanguage,
+} from "@/services/http/HttpService";
+import {
+  CourseRelationToProduct,
+  DTOCourseRelationToProduct,
+} from "@/services/api/models/Relations";
+import { DTOOrderGroup, OrderGroup } from "@/services/api/models/OrderGroup";
+import { exportToFormData } from "@/utils/forms";
+
+export const courseProductRelationsRoutes = {
+  getAll: (params: string = "") => `/course-product-relations/${params}`,
+  create: `/course-product-relations/`,
+  get: (id: string, params: string = "") =>
+    `/course-product-relations/${id}/${params}`,
+  update: (id: string) => `/course-product-relations/${id}/`,
+  delete: (id: string) => `/course-product-relations/${id}/`,
+  addOrderGroup: (id: string) =>
+    `/course-product-relations/${id}/order-groups/`,
+  editOrderGroup: (id: string, orderGroupId: string) =>
+    `/course-product-relations/${id}/order-groups/${orderGroupId}/`,
+  deleteOrderGroup: (id: string, orderGroupId: string) =>
+    `/course-product-relations/${id}/order-groups/${orderGroupId}/`,
+};
+
+export const CourseProductRelationRepository = class CourseProductRelationRepository {
+  static get(
+    id: string,
+    filters?: Maybe<ResourcesQuery>,
+  ): Promise<CourseRelationToProduct> {
+    const url = courseProductRelationsRoutes.get(
+      id,
+      filters ? `?${queryString.stringify(filters)}` : "",
+    );
+    return fetchApi(url, { method: "GET" }).then(checkStatus);
+  }
+
+  static getAll(
+    filters: Maybe<ResourcesQuery>,
+  ): Promise<CourseRelationToProduct[]> {
+    const url = courseProductRelationsRoutes.getAll(
+      filters ? `?${queryString.stringify(filters)}` : "",
+    );
+    return fetchApi(url, { method: "GET" }).then(checkStatus);
+  }
+
+  static create(
+    payload: DTOCourseRelationToProduct,
+  ): Promise<CourseRelationToProduct> {
+    return fetchApi(courseProductRelationsRoutes.create, {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(checkStatus);
+  }
+
+  static delete(id: string): Promise<void> {
+    return fetchApi(courseProductRelationsRoutes.delete(id), {
+      method: "DELETE",
+    }).then(checkStatus);
+  }
+
+  static update(
+    id: string,
+    payload: DTOCourseRelationToProduct,
+  ): Promise<CourseRelationToProduct> {
+    return fetchApi(courseProductRelationsRoutes.update(id), {
+      method: "PATCH",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json",
+        "Accept-Language": getAcceptLanguage(),
+      },
+    }).then(checkStatus);
+  }
+
+  static addOrderGroup(
+    relationId: string,
+    orderGroup: DTOOrderGroup,
+  ): Promise<OrderGroup> {
+    return fetchApi(courseProductRelationsRoutes.addOrderGroup(relationId), {
+      method: "POST",
+      body: exportToFormData(orderGroup),
+    }).then(checkStatus);
+  }
+
+  static editOrderGroup(
+    relationId: string,
+    orderGroupId: string,
+    orderGroup: DTOOrderGroup,
+  ): Promise<OrderGroup> {
+    return fetchApi(
+      courseProductRelationsRoutes.editOrderGroup(relationId, orderGroupId),
+      {
+        method: "PATCH",
+        body: exportToFormData(orderGroup),
+      },
+    ).then(checkStatus);
+  }
+  static deleteOrderGroup(
+    relationId: string,
+    orderGroupId: string,
+  ): Promise<OrderGroup> {
+    return fetchApi(
+      courseProductRelationsRoutes.deleteOrderGroup(relationId, orderGroupId),
+      {
+        method: "DELETE",
+      },
+    ).then(checkStatus);
+  }
+};
