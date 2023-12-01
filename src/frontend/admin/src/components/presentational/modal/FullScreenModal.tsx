3cfa2ed7e4b1029f142d497bdf7d37c23c69feae
nathanpanchout <nathanpanchout@live.fr>
2023-12-01 11:14:16 +0100

    ✨(frontend) adding and modifying components
    For this feature, we need to add a full page dialog modal as well as modify the
    table component to be able to select elements.

---
+++ src/frontend/admin/src/components/presentational/modal/FullScreenModal.tsx
@@ -0,0 +1,60 @@
+import Slide from "@mui/material/Slide";
+import * as React from "react";
+import { PropsWithChildren } from "react";
+import Dialog from "@mui/material/Dialog";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import { useTheme } from "@mui/material/styles";
+import Container from "@mui/material/Container";
+import { CustomModalProps } from "@/components/presentational/modal/Modal";
+
+type Props = Omit<CustomModalProps, "fullScreen" | "fullWidth"> & {};
+export function FullScreenModal({
+  disablePadding = false,
+  title,
+  open,
+  handleClose,
+  actions,
+  children,
+}: PropsWithChildren<Props>) {
+  const theme = useTheme();
+  return (
+    <Dialog
+      fullScreen
+      open={open}
+      onClose={handleClose}
+      TransitionComponent={Transition}
+    >
+      <DialogTitle bgcolor={theme.palette.grey[100]}>
+        <IconButton
+          edge="start"
+          color="inherit"
+          sx={{ mr: 1 }}
+          onClick={handleClose}
+          aria-label="close"
+        >
+          <CloseIcon />
+        </IconButton>
+        {title}
+      </DialogTitle>
+      <DialogContent sx={{ mt: 2, padding: disablePadding ? 0 : 3 }}>
+        <Container maxWidth="lg">{children}</Container>
+      </DialogContent>
+      {actions && <DialogActions>{actions}</DialogActions>}
+    </Dialog>
+  );
+}
+
+const Transition = React.forwardRef(function Transition(
+  props: PropsWithChildren<{}>,
+  ref,
+) {
+  return (
+    <Slide direction="up" ref={ref} {...props}>
+      {props.children as React.ReactElement}
+    </Slide>
+  );
+});
