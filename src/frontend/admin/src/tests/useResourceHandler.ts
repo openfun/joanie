1eaf56c7a6201770e8a45b22462dfffaa7750e4c
nathanpanchout <nathanpanchout@live.fr>
2023-12-13 10:59:17 +0100

    ✨(frontend) add playwright utils
    To make it easier to use and mock in playwright, we have added utility functions

--- src/frontend/admin/src/tests/useResourceHandler.ts
+++ src/frontend/admin/src/tests/useResourceHandler.ts
@@ -1,50 +1,92 @@
 import { Page } from "@playwright/test";
-import { useMockResource, WithId } from "@/tests/useMockResource";
+import { mockResource, WithId } from "@/tests/mockResource";
+import { DEFAULT_PAGE_SIZE } from "@/utils/constants";
 
 type Props<T extends WithId, Payload> = {
   routeUrl: string;
   page: Page;
+  searchTimeout?: number;
   updateCallback?: (payload: Payload, item?: T) => T;
   createCallback?: (payload: Payload) => T;
+  optionsResult?: any;
   searchResult?: T;
   getByIdResult?: T;
   data: T[];
 };
 
+export const getUrlCatchSearchParamsRegex = (routeUrl: string): RegExp => {
+  return new RegExp(`^${routeUrl}(\\?.*)*$`);
+};
+
+export const getUrlCatchIdRegex = (routeUrl: string): RegExp => {
+  return new RegExp(
+    `^${routeUrl}([0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12})/(\\?.*)*$`,
+  );
+};
+
+export const catchAllIdRegex = (
+  routeUrl: string,
+  keyToReplace: string,
+): RegExp => {
+  const pattern = routeUrl.replaceAll(
+    keyToReplace,
+    `([0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12})`,
+  );
+
+  return new RegExp(`^${pattern}(\\?.*)*$`);
+};
+
 export const mockPlaywrightCrud = async <T extends WithId, Payload>({
   routeUrl,
+  searchTimeout = 0,
   page,
   ...props
 }: Props<T, Payload>) => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const mockResource = useMockResource<T, Payload>({
+  const resources = mockResource<T, Payload>({
     data: props.data,
     updateCallback: props.updateCallback,
     createCallback: props.createCallback,
   });
 
-  const queryParamsRegex = new RegExp(`^${routeUrl}(\\?.*)*$`);
+  const queryParamsRegex = getUrlCatchSearchParamsRegex(routeUrl);
 
-  const catchIdRegex = new RegExp(
-    `^${routeUrl}([0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12})/(\\?.*)*$`,
-  );
+  const catchIdRegex = getUrlCatchIdRegex(routeUrl);
 
   await page.unroute(queryParamsRegex);
   await page.route(queryParamsRegex, async (route, request) => {
     const methods = request.method();
 
+    if (methods === "OPTIONS" && props.optionsResult) {
+      await route.fulfill({ json: props.optionsResult });
+    }
+
     if (methods === "GET") {
       const url = new URL(request.url());
       const query = url.searchParams.get("query");
 
-      let result = [props.searchResult ?? mockResource.data[0]];
+      let result = [props.searchResult ?? resources.data[0]];
       if (query === null || query === "") {
-        result = mockResource.data;
+        result = [...resources.data];
       }
 
       setTimeout(async () => {
-        await route.fulfill({ json: result });
-      }, 150);
+        const pageNumber = parseInt(url.searchParams.get("page") ?? "1", 10);
+        const count = result.length;
+        const index =
+          pageNumber === 1 ? 0 : (pageNumber - 1) * DEFAULT_PAGE_SIZE;
+        const listResult =
+          count <= DEFAULT_PAGE_SIZE
+            ? result
+            : result.splice(index, DEFAULT_PAGE_SIZE);
+
+        const list = {
+          count,
+          next: null,
+          previous: null,
+          results: listResult,
+        };
+        await route.fulfill({ json: list });
+      }, searchTimeout);
     }
 
     if (props.createCallback && methods === "POST") {
@@ -60,18 +102,18 @@ export const mockPlaywrightCrud = async <T extends WithId, Payload>({
     const resultMatch = request.url().match(catchIdRegex);
     const id = resultMatch?.[1] ?? "id";
     if (methods === "GET") {
-      const result = id ? mockResource.getResource(id) : props.getByIdResult;
+      const result = id ? resources.getResource(id) : props.getByIdResult;
       await route.fulfill({ json: result });
     }
 
-    if (props.updateCallback && methods === "POST") {
+    if (props.updateCallback && methods === "PATCH") {
       const payload: Payload = request.postDataJSON();
-      const update = mockResource.updateResource(id, payload);
+      const update = resources.updateResource(id, payload);
       await route.fulfill({ json: update });
     }
 
     if (methods === "DELETE") {
-      const deleteItem = mockResource.deleteResource(id);
+      const deleteItem = resources.deleteResource(id);
       await route.fulfill({ json: deleteItem });
     }
   });
