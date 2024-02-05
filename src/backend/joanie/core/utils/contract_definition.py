f3d99022fd6a611ff3396f5194d41445c549c1d4
Jonathan Reveille <jon.rev93001@gmail.com>
2024-02-05 17:48:18 +0100

    ✨(backend) contract definition context update with new properties
    Since we added new properties on the Address model and the
    Organization model, we had to update the context when
    generating the contract definition. All keys were added
    into the construction of the context. There are also default
    placeholders if there are no order for example to preview
    the contract definition template.

--- src/backend/joanie/core/utils/contract_definition.py
+++ src/backend/joanie/core/utils/contract_definition.py
@@ -4,8 +4,31 @@ from django.utils.translation import gettext as _
 
 from joanie.core.utils import image_to_base64
 
+# Organization section for generating contract definition
+ORGANIZATION_FALLBACK_ADDRESS = {
+    "address": _("<ORGANIZATION_ADDRESS_STREET_NAME>"),
+    "city": _("<ORGANIZATION_ADDRESS_CITY>"),
+    "country": _("<ORGANIZATION_ADDRESS_COUNTRY>"),
+    "last_name": _("<ORGANIZATION_LAST_NAME>"),
+    "first_name": _("<ORGANIZATION_FIRST_NAME>"),
+    "postcode": _("<ORGANIZATION_ADDRESS_POSTCODE>"),
+    "title": _("<ORGANIZATION_ADDRESS_TITLE>"),
+}
 
-# pylint: disable=import-outside-toplevel
+# Student section for generating contract definition
+USER_FALLBACK_ADDRESS = {
+    "address": _("<STUDENT_ADDRESS_STREET_NAME>"),
+    "city": _("<STUDENT_ADDRESS_CITY>"),
+    "country": _("<STUDENT_ADDRESS_COUNTRY>"),
+    "last_name": _("<STUDENT_LAST_NAME>"),
+    "first_name": _("<STUDENT_FIRST_NAME>"),
+    "postcode": _("<STUDENT_ADDRESS_POSTCODE>"),
+    "title": _("<STUDENT_ADDRESS_TITLE>"),
+}
+
+
+# ruff: noqa: PLR0915
+# pylint: disable=import-outside-toplevel, too-many-locals, too-many-statements
 def generate_document_context(contract_definition, user, order=None):
     """
     Generate a document context for a contract definition.
@@ -16,28 +39,14 @@ def generate_document_context(contract_definition, user, order=None):
     We can use this method to preview a contract definition, or to generate its context when all
     the parameters are set.
     """
-    from joanie.core.serializers.client import (  # pylint: disable=cyclic-import
-        AddressSerializer,
-    )
+    # pylint: disable=cyclic-import
+    from joanie.core.models import Address
+    from joanie.core.serializers.client import AddressSerializer
 
     organization_fallback_logo = (
         "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR"
         "42mO8cPX6fwAIdgN9pHTGJwAAAABJRU5ErkJggg=="
     )
-
-    fallback_address = {
-        "address": _("<STUDENT_ADDRESS_STREET_NAME>"),
-        "city": _("<STUDENT_ADDRESS_CITY>"),
-        "country": _("<STUDENT_ADDRESS_COUNTRY>"),
-        "last_name": _("<STUDENT_LAST_NAME>"),
-        "first_name": _("<STUDENT_FIRST_NAME>"),
-        "postcode": _("<STUDENT_ADDRESS_POSTCODE>"),
-        "title": "",
-    }
-
-    user_address = order.main_invoice.recipient_address if order else fallback_address
-
-    address = AddressSerializer(user_address).data
 
     try:
         site_config = Site.objects.get_current().site_config
@@ -48,6 +57,68 @@ def generate_document_context(contract_definition, user, order=None):
             contract_definition.language
         )
 
+    organization_logo = organization_fallback_logo
+    organization_name = _("<ORGANIZATION_NAME>")
+    organization_representative = _("<REPRESENTATIVE>")
+    organization_representative_profession = _("<REPRESENTATIVE_PROFESSION>")
+    organization_enterprise_code = _("<ENTERPRISE_CODE>")
+    organization_activity_category_code = _("<ACTIVITY_CATEGORY_CODE>")
+    organization_signatory_representative = _("<SIGNATORY_REPRESENTATIVE>")
+    organization_signatory_profession = _("<SIGNATURE_REPRESENTATIVE_PROFESSION>")
+    organization_contact_phone = _("<CONTACT_PHONE>")
+    organization_contact_email = _("<CONTACT_EMAIL>")
+    organization_dpo_email = _("<DPO_EMAIL_ADDRESS>")
+    organization_address = ORGANIZATION_FALLBACK_ADDRESS
+
+    course_code = _("<COURSE_CODE>")
+    course_name = _("<COURSE_NAME>")
+    course_start = _("<COURSE_START_DATE>")
+    course_end = _("<COURSE_END_DATE>")
+    course_price = _("<PRICE>")
+
+    user_address = USER_FALLBACK_ADDRESS
+
+    if order:
+        organization_logo = image_to_base64(order.organization.logo)
+        organization_name = order.organization.safe_translation_getter(
+            "title", language_code=contract_definition.language
+        )
+        organization_representative = order.organization.representative
+        organization_representative_profession = (
+            order.organization.representative_profession
+        )
+        organization_enterprise_code = order.organization.enterprise_code
+        organization_activity_category_code = order.organization.activity_category_code
+        organization_signatory_representative = (
+            order.organization.signatory_representative
+        )
+        organization_signatory_profession = (
+            order.organization.signatory_representative_profession
+        )
+        organization_contact_phone = order.organization.contact_phone
+        organization_contact_email = order.organization.contact_email
+        organization_dpo_email = order.organization.dpo_email
+
+        try:
+            organization_address = order.organization.addresses.get(is_main=True)
+        except Address.DoesNotExist:
+            organization_address = ORGANIZATION_FALLBACK_ADDRESS
+
+        # Course
+        course_code = order.course.code
+        course_name = order.product.safe_translation_getter(
+            "title", language_code=contract_definition.language
+        )
+        course_dates = order.get_equivalent_course_run_dates()
+        course_start = course_dates["start"]
+        course_end = course_dates["end"]
+        course_price = str(order.total)
+
+        user_address = order.main_invoice.recipient_address
+
+    address_organization = AddressSerializer(organization_address).data
+    address_user = AddressSerializer(user_address).data
+
     return {
         "contract": {
             "body": contract_definition.get_body_in_html(),
@@ -55,27 +126,31 @@ def generate_document_context(contract_definition, user, order=None):
             "title": contract_definition.title,
         },
         "course": {
-            "name": (
-                order.product.safe_translation_getter(
-                    "title", language_code=contract_definition.language
-                )
-                if order
-                else _("<COURSE_NAME>")
-            )
+            "name": course_name,
+            "code": course_code,
+            "start": course_start,
+            "end": course_end,
+            "effort": None,
+            "price": course_price,
         },
-        "student": {"name": user.get_full_name() or user.username, "address": address},
+        "student": {
+            "address": address_user,
+            "name": user.get_full_name() or user.username,
+            "email": (user.email if user else _("<USER_EMAIL>")),
+            "phone_number": None,
+        },
         "organization": {
-            "logo": (
-                image_to_base64(order.organization.logo)
-                if order
-                else organization_fallback_logo
-            ),
-            "name": (
-                order.organization.safe_translation_getter(
-                    "title", language_code=contract_definition.language
-                )
-                if order
-                else _("<ORGANIZATION_NAME>")
-            ),
+            "address": address_organization,
+            "logo": organization_logo,
+            "name": organization_name,
+            "representative": organization_representative,
+            "representative_profession": organization_representative_profession,
+            "enterprise_code": organization_enterprise_code,
+            "activity_category_code": organization_activity_category_code,
+            "signatory_representative": organization_signatory_representative,
+            "signatory_representative_profession": organization_signatory_profession,
+            "contact_phone": organization_contact_phone,
+            "contact_email": organization_contact_email,
+            "dpo_email": organization_dpo_email,
         },
     }
--- src/backend/joanie/tests/core/test_utils_contract_definition_generate_document_context.py
+++ src/backend/joanie/tests/core/test_utils_contract_definition_generate_document_context.py
@@ -10,6 +10,8 @@ from joanie.payment.factories import InvoiceFactory
 class UtilsGenerateDocumentContextTestCase(TestCase):
     """Test suite for `generate_document_context` utility"""
 
+    maxDiff = None
+
     def test_utils_contract_definition_generate_document_context_with_order(self):
         """
         When we generate the document context for a contract definition with an order,
@@ -17,13 +19,15 @@ class UtilsGenerateDocumentContextTestCase(TestCase):
         the user, the order and the terms and conditions of the current site.
         """
         user = factories.UserFactory(
-            email="student@exmaple.fr", first_name="John Doe", last_name=""
+            email="johndoe@example.fr",
+            first_name="John Doe",
+            last_name="",
         )
         factories.SiteConfigFactory(
             site=Site.objects.get_current(),
             terms_and_conditions="## Terms and conditions",
         )
-        address = factories.UserAddressFactory(
+        user_address = factories.UserAddressFactory(
             owner=user,
             first_name="John",
             last_name="Doe",
@@ -34,17 +38,39 @@ class UtilsGenerateDocumentContextTestCase(TestCase):
             title="Office",
             is_main=False,
         )
+        organization = factories.OrganizationFactory(
+            dpo_email="johnnydoes@example.fr",
+            contact_email="contact@example.fr",
+            contact_phone="0123456789",
+            enterprise_code="1234",
+            activity_category_code="abcd1234",
+            representative="Mister Example",
+            representative_profession="Educational representative",
+            signatory_representative="Big boss",
+            signatory_representative_profession="Director",
+        )
+        address_organization = factories.OrganizationAddressFactory(
+            organization=organization, is_main=True, is_reusable=True
+        )
+        relation = factories.CourseProductRelationFactory(
+            organizations=[organization],
+            product__contract_definition=factories.ContractDefinitionFactory(
+                title="CONTRACT DEFINITION 1",
+                body="Articles de la convention",
+            ),
+            course=factories.CourseFactory(organizations=[organization]),
+        )
         order = factories.OrderFactory(
             owner=user,
-            product=factories.ProductFactory(
-                contract_definition=factories.ContractDefinitionFactory(
-                    title="CONTRACT DEFINITION 1", body="Articles de la convention"
-                )
-            ),
+            product=relation.product,
+            course=relation.course,
             state=enums.ORDER_STATE_VALIDATED,
-            main_invoice=InvoiceFactory(recipient_address=address),
+            main_invoice=InvoiceFactory(recipient_address=user_address),
         )
-        definition = order.product.contract_definition
+        factories.OrderTargetCourseRelationFactory(
+            course=relation.course, order=order, position=1
+        )
+        freezed_course_data = order.get_equivalent_course_run_dates()
         expected_context = {
             "contract": {
                 "body": "<p>Articles de la convention</p>",
@@ -53,29 +79,60 @@ class UtilsGenerateDocumentContextTestCase(TestCase):
             },
             "course": {
                 "name": order.product.title,
+                "code": relation.course.code,
+                "start": freezed_course_data["start"],
+                "end": freezed_course_data["end"],
+                "effort": None,
+                "price": str(order.total),
             },
             "student": {
                 "name": user.get_full_name(),
                 "address": {
-                    "address": "5 Rue de L'Exemple",
-                    "city": "Paris",
-                    "country": "FR",
-                    "first_name": "John",
-                    "last_name": "Doe",
-                    "id": str(address.id),
-                    "is_main": False,
-                    "postcode": "75000",
-                    "title": "Office",
+                    "id": str(user_address.id),
+                    "address": user_address.address,
+                    "city": user_address.city,
+                    "country": str(user_address.country),
+                    "last_name": user_address.last_name,
+                    "first_name": user_address.first_name,
+                    "postcode": user_address.postcode,
+                    "title": user_address.title,
+                    "is_main": user_address.is_main,
                 },
+                "email": user.email,
+                "phone_number": None,
             },
             "organization": {
+                "address": {
+                    "id": str(address_organization.id),
+                    "address": address_organization.address,
+                    "city": address_organization.city,
+                    "country": str(address_organization.country),
+                    "last_name": address_organization.last_name,
+                    "first_name": address_organization.first_name,
+                    "postcode": address_organization.postcode,
+                    "title": address_organization.title,
+                    "is_main": address_organization.is_main,
+                },
                 "logo": image_to_base64(order.organization.logo),
-                "name": order.organization.title,
+                "name": organization.title,
+                "representative": organization.representative,
+                "representative_profession": organization.representative_profession,
+                "enterprise_code": organization.enterprise_code,
+                "activity_category_code": organization.activity_category_code,
+                "signatory_representative": organization.signatory_representative,
+                "signatory_representative_profession": (
+                    organization.signatory_representative_profession
+                ),
+                "contact_phone": organization.contact_phone,
+                "contact_email": organization.contact_email,
+                "dpo_email": organization.dpo_email,
             },
         }
 
         context = contract_definition.generate_document_context(
-            contract_definition=definition, user=user, order=order
+            contract_definition=order.product.contract_definition,
+            user=user,
+            order=order,
         )
 
         self.assertDictEqual(context, expected_context)
@@ -91,7 +148,9 @@ class UtilsGenerateDocumentContextTestCase(TestCase):
             "42mO8cPX6fwAIdgN9pHTGJwAAAABJRU5ErkJggg=="
         )
         user = factories.UserFactory(
-            email="student@exmaple.fr", first_name="John Doe", last_name=""
+            email="student@example.fr",
+            first_name="John Doe",
+            last_name="",
         )
         definition = factories.ContractDefinitionFactory(
             title="CONTRACT DEFINITION 2", body="Articles de la convention"
@@ -104,6 +163,11 @@ class UtilsGenerateDocumentContextTestCase(TestCase):
             },
             "course": {
                 "name": "<COURSE_NAME>",
+                "code": "<COURSE_CODE>",
+                "start": "<COURSE_START_DATE>",
+                "end": "<COURSE_END_DATE>",
+                "effort": None,
+                "price": "<PRICE>",
             },
             "student": {
                 "name": user.get_full_name(),
@@ -114,12 +178,32 @@ class UtilsGenerateDocumentContextTestCase(TestCase):
                     "last_name": "<STUDENT_LAST_NAME>",
                     "first_name": "<STUDENT_FIRST_NAME>",
                     "postcode": "<STUDENT_ADDRESS_POSTCODE>",
-                    "title": "",
+                    "title": "<STUDENT_ADDRESS_TITLE>",
                 },
+                "email": "student@example.fr",
+                "phone_number": None,
             },
             "organization": {
+                "address": {
+                    "address": "<ORGANIZATION_ADDRESS_STREET_NAME>",
+                    "city": "<ORGANIZATION_ADDRESS_CITY>",
+                    "country": "<ORGANIZATION_ADDRESS_COUNTRY>",
+                    "last_name": "<ORGANIZATION_LAST_NAME>",
+                    "first_name": "<ORGANIZATION_FIRST_NAME>",
+                    "postcode": "<ORGANIZATION_ADDRESS_POSTCODE>",
+                    "title": "<ORGANIZATION_ADDRESS_TITLE>",
+                },
                 "logo": organization_fallback_logo,
                 "name": "<ORGANIZATION_NAME>",
+                "representative": "<REPRESENTATIVE>",
+                "representative_profession": "<REPRESENTATIVE_PROFESSION>",
+                "enterprise_code": "<ENTERPRISE_CODE>",
+                "activity_category_code": "<ACTIVITY_CATEGORY_CODE>",
+                "signatory_representative": "<SIGNATORY_REPRESENTATIVE>",
+                "signatory_representative_profession": "<SIGNATURE_REPRESENTATIVE_PROFESSION>",
+                "contact_phone": "<CONTACT_PHONE>",
+                "contact_email": "<CONTACT_EMAIL>",
+                "dpo_email": "<DPO_EMAIL_ADDRESS>",
             },
         }
 
@@ -134,45 +218,85 @@ class UtilsGenerateDocumentContextTestCase(TestCase):
     ):
         """
         When we generate the document context for the contract definition without : an order and
-        an address, it should return the default placeholder values for the fields :
-        `course.name`, `student.address`, `organization.logo`, `organization.signature`,
-        `organization.title`.
+        an address, it should return the default placeholder values for different sections of the
+        context :
+
+        For course :
+        `course.start`, `course.end`, `course.effort`, `course.price`
+        `course.name`.
+
+        For student :
+        `student.address`, `student.phone_number`.
+
+        For organization :
+        `organization.address`, `organization.logo`, `organization.signature`,
+        `organization.title`, `organization.representative`,
+        `organization.enterprise_code`, `organization.activity_category_code`
+        `organization.signatory_representative`, `organization.contact_phone`,
+        `organization.signatory_representative_profession`, `organization.contact_email`
+        `organization.dpo_email`, `organization.representative_profession`.
         """
         organization_fallback_logo = (
             "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR"
             "42mO8cPX6fwAIdgN9pHTGJwAAAABJRU5ErkJggg=="
         )
-        student_fallback_address = {
-            "address": "<STUDENT_ADDRESS_STREET_NAME>",
-            "city": "<STUDENT_ADDRESS_CITY>",
-            "country": "<STUDENT_ADDRESS_COUNTRY>",
-            "last_name": "<STUDENT_LAST_NAME>",
-            "first_name": "<STUDENT_FIRST_NAME>",
-            "postcode": "<STUDENT_ADDRESS_POSTCODE>",
-            "title": "",
-        }
         user = factories.UserFactory(
-            email="student@exmaple.fr", first_name="John Doe", last_name=""
+            email="student@example.fr",
+            first_name="John Doe",
+            last_name="",
         )
         definition = factories.ContractDefinitionFactory(
-            title="CONTRACT DEFINITION 2", body="Articles de la convention"
+            title="CONTRACT DEFINITION 3", body="Articles de la convention"
         )
         expected_context = {
             "contract": {
                 "body": "<p>Articles de la convention</p>",
                 "terms_and_conditions": "",
-                "title": "CONTRACT DEFINITION 2",
+                "title": "CONTRACT DEFINITION 3",
             },
             "course": {
                 "name": "<COURSE_NAME>",
+                "code": "<COURSE_CODE>",
+                "start": "<COURSE_START_DATE>",
+                "end": "<COURSE_END_DATE>",
+                "effort": None,
+                "price": "<PRICE>",
             },
             "student": {
                 "name": user.get_full_name(),
-                "address": student_fallback_address,
+                "address": {
+                    "address": "<STUDENT_ADDRESS_STREET_NAME>",
+                    "city": "<STUDENT_ADDRESS_CITY>",
+                    "country": "<STUDENT_ADDRESS_COUNTRY>",
+                    "last_name": "<STUDENT_LAST_NAME>",
+                    "first_name": "<STUDENT_FIRST_NAME>",
+                    "postcode": "<STUDENT_ADDRESS_POSTCODE>",
+                    "title": "<STUDENT_ADDRESS_TITLE>",
+                },
+                "email": "student@example.fr",
+                "phone_number": None,
             },
             "organization": {
+                "address": {
+                    "address": "<ORGANIZATION_ADDRESS_STREET_NAME>",
+                    "city": "<ORGANIZATION_ADDRESS_CITY>",
+                    "country": "<ORGANIZATION_ADDRESS_COUNTRY>",
+                    "last_name": "<ORGANIZATION_LAST_NAME>",
+                    "first_name": "<ORGANIZATION_FIRST_NAME>",
+                    "postcode": "<ORGANIZATION_ADDRESS_POSTCODE>",
+                    "title": "<ORGANIZATION_ADDRESS_TITLE>",
+                },
                 "logo": organization_fallback_logo,
                 "name": "<ORGANIZATION_NAME>",
+                "representative": "<REPRESENTATIVE>",
+                "representative_profession": "<REPRESENTATIVE_PROFESSION>",
+                "enterprise_code": "<ENTERPRISE_CODE>",
+                "activity_category_code": "<ACTIVITY_CATEGORY_CODE>",
+                "signatory_representative": "<SIGNATORY_REPRESENTATIVE>",
+                "signatory_representative_profession": "<SIGNATURE_REPRESENTATIVE_PROFESSION>",
+                "contact_phone": "<CONTACT_PHONE>",
+                "contact_email": "<CONTACT_EMAIL>",
+                "dpo_email": "<DPO_EMAIL_ADDRESS>",
             },
         }
 
