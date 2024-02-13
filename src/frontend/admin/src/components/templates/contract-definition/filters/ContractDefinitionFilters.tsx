7bb256977219244d891c05c03611cbfbb6f10a92
NathanPanchout <nathanpanchout@hotmail.com>
2024-02-13 17:44:53 +0100

    ✨(frontend) create all filters forms
    It is now time to create the different filter forms according to the entities.

---
+++ src/frontend/admin/src/components/templates/contract-definition/filters/ContractDefinitionFilters.tsx
@@ -0,0 +1,74 @@
+import * as React from "react";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Grid from "@mui/material/Unstable_Grid2";
+import { defineMessages, useIntl } from "react-intl";
+import {
+  MandatorySearchFilterProps,
+  SearchFilters,
+} from "@/components/presentational/filters/SearchFilters";
+import { RHFProvider } from "@/components/presentational/hook-form/RHFProvider";
+import { RHFValuesChange } from "@/components/presentational/hook-form/RFHValuesChange";
+import { RHFContractDefinitionLanguage } from "@/components/templates/contract-definition/inputs/RHFContractDefinitionLanguage";
+
+const messages = defineMessages({
+  searchPlaceholder: {
+    id: "components.templates.contractDefinition.filters.ContractDefinitionFilters.searchPlaceholder",
+    description: "Text for the search input placeholder",
+    defaultMessage: "Search by title",
+  },
+});
+
+type Props = MandatorySearchFilterProps & {
+  onFilter: (values: any) => void;
+};
+
+export function ContractDefinitionFilters({
+  onFilter,
+  ...searchFilterProps
+}: Props) {
+  const intl = useIntl();
+  const getDefaultValues = () => {
+    return {
+      language: "",
+    };
+  };
+  const RegisterSchema = Yup.object().shape({
+    language: Yup.string().nullable(),
+  });
+
+  const methods = useForm({
+    resolver: yupResolver(RegisterSchema),
+    defaultValues: getDefaultValues() as any, // To not trigger type validation for default value
+  });
+
+  return (
+    <SearchFilters
+      {...searchFilterProps}
+      searchInputPlaceholder={intl.formatMessage(messages.searchPlaceholder)}
+      renderContent={() => (
+        <RHFProvider
+          id="cotract-definition-filter-form"
+          showSubmit={false}
+          methods={methods}
+        >
+          <RHFValuesChange
+            debounceTime={200}
+            useAnotherValueReference={true}
+            onSubmit={onFilter}
+          >
+            <Grid container mt={2} spacing={2}>
+              <Grid xs={12}>
+                <RHFContractDefinitionLanguage
+                  isFilterContext={true}
+                  name="language"
+                />
+              </Grid>
+            </Grid>
+          </RHFValuesChange>
+        </RHFProvider>
+      )}
+    />
+  );
+}
---
+++ src/frontend/admin/src/components/templates/courses/filters/CourseFilters.tsx
@@ -0,0 +1,91 @@
+import * as React from "react";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Grid from "@mui/material/Unstable_Grid2";
+import { defineMessages, useIntl } from "react-intl";
+import { OrganizationSearch } from "../../organizations/inputs/search/OrganizationSearch";
+import { courseFormMessages } from "../form/translations";
+import {
+  MandatorySearchFilterProps,
+  SearchFilters,
+} from "@/components/presentational/filters/SearchFilters";
+import { RHFProvider } from "@/components/presentational/hook-form/RHFProvider";
+import { RHFValuesChange } from "@/components/presentational/hook-form/RFHValuesChange";
+
+import { Organization } from "@/services/api/models/Organization";
+import { CourseResourceQuery } from "@/hooks/useCourses/useCourses";
+
+const messages = defineMessages({
+  searchPlaceholder: {
+    id: "components.templates.courses.filters.CourseFilters.searchPlaceholder",
+    description: "Text for the search input placeholder",
+    defaultMessage: "Search by title or code",
+  },
+});
+
+type FormValues = {
+  organizations?: Organization[];
+};
+
+type Props = MandatorySearchFilterProps & {
+  onFilter: (values: CourseResourceQuery) => void;
+};
+
+export function CourseFilters({ onFilter, ...searchFilterProps }: Props) {
+  const intl = useIntl();
+  const getDefaultValues = () => {
+    return {
+      organizations: [],
+    };
+  };
+  const RegisterSchema = Yup.object().shape({
+    organizations: Yup.array<any, Organization>().nullable(),
+  });
+
+  const methods = useForm({
+    resolver: yupResolver(RegisterSchema),
+    defaultValues: getDefaultValues() as any, // To not trigger type validation for default value
+  });
+
+  const onSubmit = (values: FormValues) => {
+    const filters: CourseResourceQuery = {
+      organization_ids: values.organizations?.map((org) => org.id),
+    };
+    onFilter(filters);
+  };
+
+  return (
+    <SearchFilters
+      {...searchFilterProps}
+      searchInputPlaceholder={intl.formatMessage(messages.searchPlaceholder)}
+      renderContent={() => (
+        <RHFProvider
+          id="course-filter-form"
+          showSubmit={false}
+          methods={methods}
+        >
+          <RHFValuesChange
+            debounceTime={200}
+            useAnotherValueReference={true}
+            onSubmit={onSubmit}
+          >
+            <Grid container mt={2} spacing={2}>
+              <Grid xs={12}>
+                <OrganizationSearch
+                  enableAdd={false}
+                  multiple={true}
+                  isFilterContext={true}
+                  name="organizations"
+                  label={intl.formatMessage(
+                    courseFormMessages.organizationsLabel,
+                  )}
+                />
+              </Grid>
+            </Grid>
+          </RHFValuesChange>
+        </RHFProvider>
+      )}
+    />
+  );
+}
