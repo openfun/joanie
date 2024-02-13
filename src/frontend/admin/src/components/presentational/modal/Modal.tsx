1810bbc641b60f8044d2cc91a14dc343ccebd9a3
NathanPanchout <nathanpanchout@hotmail.com>
2024-02-13 17:25:29 +0100

    💄(fronted) add close button for modal
    Currently, there is no way to close a modal without clicking outside of it.
    It is not easy to understand this mechanism for everyone.

--- src/frontend/admin/src/components/presentational/modal/Modal.tsx
+++ src/frontend/admin/src/components/presentational/modal/Modal.tsx
@@ -4,6 +4,8 @@ import Dialog, { DialogProps } from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 export interface CustomModalProps extends DialogProps {
   title: string;
@@ -29,6 +31,18 @@ export function CustomModal({
   return (
     <Dialog {...props} open={open} onClose={handleClose}>
       <DialogTitle>{title}</DialogTitle>
+      <IconButton
+        aria-label="close"
+        onClick={handleClose}
+        sx={{
+          position: "absolute",
+          right: 8,
+          top: 8,
+          color: (theme) => theme.palette.grey[500],
+        }}
+      >
+        <CloseIcon />
+      </IconButton>
       <DialogContent sx={{ padding: disablePadding ? 0 : 3 }}>
         {children}
       </DialogContent>
