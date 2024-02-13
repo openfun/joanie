0d5e555b0decc929a925cec4232a54cbfd524bb7
NathanPanchout <nathanpanchout@hotmail.com>
2024-02-13 17:38:26 +0100

    🌐(frontend) add many translations
    We need several translations, such as the translation of priorities

--- src/frontend/admin/src/translations/common/languageTranslations.ts
+++ src/frontend/admin/src/translations/common/languageTranslations.ts
@@ -1,12 +1,13 @@
 import { defineMessages } from "react-intl";
+import { LocalesEnum } from "@/types/i18n/LocalesEnum";
 
-export const languageTranslations = defineMessages({
-  english: {
+export const languageTranslations = defineMessages<LocalesEnum>({
+  [LocalesEnum.ENGLISH]: {
     id: "translations.common.languageTranslations.english",
     defaultMessage: "English",
     description: "Common english label",
   },
-  french: {
+  [LocalesEnum.FRENCH]: {
     id: "translations.common.languageTranslations.french",
     defaultMessage: "French",
     description: "Common french label",
