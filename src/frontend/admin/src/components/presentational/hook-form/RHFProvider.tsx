4e8ff8d2fc92b9caf162717ecabb3176456a0663
NathanPanchout <nathanpanchout@hotmail.com>
2024-02-21 13:59:05 +0100

    ✨(frontend) add an alert for unsaved forms
    Currently, if you have an unsaved modified form and you want to change pages or
    refresh the page, no message warns you that the form is not saved

--- src/frontend/admin/src/components/presentational/hook-form/RHFProvider.tsx
+++ src/frontend/admin/src/components/presentational/hook-form/RHFProvider.tsx
@@ -1,12 +1,26 @@
 import * as React from "react";
-import { MutableRefObject } from "react";
+import { MutableRefObject, useEffect, useState } from "react";
 import { FormProvider, UseFormReturn } from "react-hook-form";
 import { FieldValues } from "react-hook-form/dist/types/fields";
 import Box from "@mui/material/Box";
-import { defineMessages, FormattedMessage } from "react-intl";
+import { defineMessages, FormattedMessage, useIntl } from "react-intl";
 import LoadingButton from "@mui/lab/LoadingButton";
+import { useRouter } from "next/router";
+import { AlertModal } from "@/components/presentational/modal/AlertModal";
+import { useModal } from "@/components/presentational/modal/useModal";
 
 const messages = defineMessages({
+  dirtyModalTitle: {
+    id: "components.presentational.hookForm.provider.dirtyModalTitle",
+    defaultMessage: "Before you leave!",
+    description: "Title for the dirty alert modal",
+  },
+  dirtyModalMessage: {
+    id: "components.presentational.hookForm.provider.dirtyModalMessage",
+    defaultMessage:
+      "Changes in the form have been detected. Please submit the form or your changes will be lost.",
+    description: "Message for the dirty alert modal",
+  },
   submit: {
     id: "components.presentational.hookForm.provider.submit",
     defaultMessage: "Submit",
@@ -23,6 +37,7 @@ interface Props<T extends FieldValues> {
   formRef?: MutableRefObject<HTMLFormElement | undefined>;
   isSubmitting?: boolean;
   showSubmit?: boolean;
+  checkBeforeUnload?: boolean;
 }
 
 export function RHFProvider<T extends FieldValues>({
@@ -32,8 +47,62 @@ export function RHFProvider<T extends FieldValues>({
   actionButtons,
   showSubmit = true,
   isSubmitting = false,
+  checkBeforeUnload = false,
   id,
 }: Props<T>) {
+  const intl = useIntl();
+  const router = useRouter();
+  const [nextUrl, setNextUrl] = useState<string>();
+  const dirtyModal = useModal();
+
+  useEffect(() => {
+    const beforeUnload = (event: BeforeUnloadEvent) => {
+      if (!methods.formState.isDirty) {
+        return;
+      }
+
+      event.preventDefault();
+    };
+
+    window.addEventListener("beforeunload", beforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", beforeUnload);
+    };
+  }, [methods.formState.isDirty]);
+
+  useEffect(() => {
+    const routeChangeStart = (url: string) => {
+      if (
+        !checkBeforeUnload ||
+        !methods.formState.isDirty ||
+        methods.formState.isSubmitted ||
+        methods.formState.isSubmitting ||
+        (nextUrl && dirtyModal.open)
+      ) {
+        return;
+      }
+      setNextUrl(url);
+      dirtyModal.handleOpen();
+
+      router.events.emit("routeChangeError");
+      // eslint-disable-next-line @typescript-eslint/no-throw-literal
+      throw `Form is dirty`;
+    };
+    router.events.on("routeChangeStart", routeChangeStart);
+    return () => {
+      router.events.off("routeChangeStart", routeChangeStart);
+    };
+  }, [
+    nextUrl,
+    methods.formState.isSubmitted,
+    methods.formState.submitCount,
+    methods.formState.isSubmitting,
+    methods.formState.isSubmitting,
+    methods.formState.isDirty,
+    isSubmitting,
+    dirtyModal.open,
+  ]);
+
   return (
     <FormProvider {...methods}>
       <form
@@ -67,6 +136,24 @@ export function RHFProvider<T extends FieldValues>({
           </Box>
         )}
       </form>
+      <AlertModal
+        open={dirtyModal.open}
+        closeOnAccept={false}
+        maxWidth="md"
+        fullWidth={true}
+        title={intl.formatMessage(messages.dirtyModalTitle)}
+        message={intl.formatMessage(messages.dirtyModalMessage)}
+        handleAccept={() => {
+          if (!nextUrl) {
+            return;
+          }
+          router.push(nextUrl);
+        }}
+        handleClose={() => {
+          setNextUrl(undefined);
+          dirtyModal.handleClose();
+        }}
+      />
     </FormProvider>
   );
 }
