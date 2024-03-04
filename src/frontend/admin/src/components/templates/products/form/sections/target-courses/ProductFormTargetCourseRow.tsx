9b52af5e22e14f5e982bfbe8518fe3bfa23ab7f8
NathanPanchout <nathanpanchout@hotmail.com>
2024-03-04 08:56:02 +0100

    ✨(frontend) make target courses clickable
    Currently, it is not possible to click on course inside the
    course product relation card

--- src/frontend/admin/src/components/templates/products/form/sections/target-courses/ProductFormTargetCourseRow.tsx
+++ src/frontend/admin/src/components/templates/products/form/sections/target-courses/ProductFormTargetCourseRow.tsx
@@ -7,6 +7,8 @@ import {
 import { productFormMessages } from "@/components/templates/products/form/translations";
 import { DndDefaultRowProps } from "@/components/presentational/dnd/DndDefaultRow";
 import { DefaultRow } from "@/components/presentational/list/DefaultRow";
+import { CustomLink } from "@/components/presentational/link/CustomLink";
+import { PATH_ADMIN } from "@/utils/routes/path";
 
 type Props = Omit<DndDefaultRowProps, "mainTitle"> & {
   item: ProductTargetCourseRelation | ProductTargetCourseRelationOptionalId;
@@ -30,7 +32,11 @@ export function ProductFormTargetCourseRow({ item, ...props }: Props) {
       }`}
       enableEdit={item.id !== undefined}
       enableDelete={item.id !== undefined}
-      mainTitle={item.course?.title}
+      mainTitle={
+        <CustomLink href={PATH_ADMIN.courses.edit(item.course.id)}>
+          {item.course.title}
+        </CustomLink>
+      }
       subTitle={intl.formatMessage(
         productFormMessages.targetCourseRowSubTitle,
         { numPhotos: item.course_runs.length },
