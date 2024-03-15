84633d4c001ff8d443b685b51e6dc66aadcf01ba
Jonathan Reveille <jon.rev93001@gmail.com>
2024-03-15 17:44:41 +0100

    ✨(backend) generate certificate for backoffice from a course
    New endpoint to allow the backoffice staff user to generate
    certificate from a course view, and launch the generation of
    certificate from its relation to product. To avoid having a heavy
    request, we decided to use celery to execute the task in background.

--- src/backend/joanie/core/tasks.py
+++ src/backend/joanie/core/tasks.py
@@ -2,9 +2,11 @@
 
 from logging import getLogger
 
+from django.core.cache import cache
 from django.core.management import call_command
 
 from joanie.celery_app import app
+from joanie.core import helpers
 
 logger = getLogger(__name__)
 
@@ -19,3 +21,17 @@ def generate_zip_archive_task(options):
     logger.info("Starting Celery task, generating a ZIP Archive...")
     call_command("generate_zip_archive_of_contracts", **options)
     logger.info("Done executing Celery generating a ZIP Archive task...")
+
+
+@app.task
+def generate_certificates_task(order_ids, cache_key):
+    """
+    Task to generate certificates from orders. It calls the helper command to generate
+    certificates from an Order queryset. Once done, it cleans up the data in cache.
+    """
+    logger.info("Starting Celery task, generating certificates...")
+    try:
+        helpers.generate_certificates_for_orders(orders=order_ids)
+    finally:
+        cache.delete(cache_key)
+    logger.info("Done executing Celery generating certificates task...")
