7d867b3c81dd767b0efc1fbe8ffd4bba8c64b234
Nicolas Clerc <kernicpanel@nclerc.fr>
2024-04-12 17:04:33 +0200

    ✨(lyra) add payment webhook
    When a payment is triggered by the lyra frontend form, we get the
    payment status through a webhook.

--- src/backend/joanie/payment/backends/lyra/__init__.py
+++ src/backend/joanie/payment/backends/lyra/__init__.py
@@ -173,3 +173,97 @@ class LyraBackend(BasePaymentBackend):
         payload = self._get_common_payload_data(order, billing_address)
         payload["formAction"] = "ASK_REGISTER_PAY"
         return self._get_form_token(url, payload)
+
+    def _check_hash(self, post_data):
+        """Verify IPN authenticity"""
+        kr_answer = post_data["kr-answer"].encode("utf-8")
+        secret_key = self.configuration["password"].encode("utf-8")
+        hashed = hmac.new(secret_key, kr_answer, hashlib.sha256)
+        return hashed.hexdigest() == post_data["kr-hash"]
+
+    def handle_notification(self, request):
+        """
+        Handle the notification from Lyra
+
+        https://docs.lyra.com/fr/rest/V4.0/api/kb/ipn.html#quel-est-le-principe-de-fonctionnement-dune-ipn-header
+        https://docs.lyra.com/fr/rest/V4.0/api/kb/ipn_usage.html
+        """
+        request.parsers = self.parser_classes
+        post_data = request.POST.dict()
+
+        if not self._check_hash(post_data):
+            raise exceptions.ParseNotificationFailed()
+
+        if settings.DEBUG:
+            with open("request.json", "w", encoding="utf-8") as f:
+                json.dump(post_data, f, indent=4)
+
+        answer = json.loads(post_data.get("kr-answer"))
+
+        if settings.DEBUG:
+            with open("answer.json", "w", encoding="utf-8") as f:
+                json.dump(answer, f, indent=4)
+
+        transaction_id = answer["transactions"][0]["uuid"]
+        try:
+            order_id = answer["orderDetails"]["orderId"]
+        except KeyError as error:
+            logger.error(
+                "Received notification for payment %s without order ID.", transaction_id
+            )
+            raise exceptions.ParseNotificationFailed() from error
+
+        try:
+            order = Order.objects.get(id=order_id)
+        except Order.DoesNotExist as error:
+            logger.error(
+                (
+                    "Received notification for payment %s related to "
+                    "a non existing order #%s."
+                ),
+                transaction_id,
+                order_id,
+            )
+            raise exceptions.RegisterPaymentFailed(
+                f"Payment {transaction_id} relies on a non-existing order ({order_id})."
+            ) from error
+
+        card_token = answer["transactions"][0]["paymentMethodToken"]
+        card_details = answer["transactions"][0]["transactionDetails"]["cardDetails"]
+        card_pan = card_details["pan"]
+        # Register card if user has requested it
+        if card_token is not None and card_pan is not None:
+            # In the case of a one click payment, card.id is not None but other
+            # attributes are empty. So to know if a user wants to save its card,
+            # we check if card.id and one other card attribute are not None.
+            # - User asks to store its card
+            CreditCard.objects.create(
+                brand=card_details["effectiveBrand"],
+                expiration_month=card_details["expiryMonth"],
+                expiration_year=card_details["expiryYear"],
+                last_numbers=card_pan[-4:],  # last 4 digits
+                owner=order.owner,
+                token=card_token,
+            )
+
+        if answer["orderStatus"] == "PAID":
+            billing_details = answer["customer"]["billingDetails"]
+            self._do_on_payment_success(
+                order=order,
+                payment={
+                    "id": transaction_id,
+                    "amount": D(
+                        f"{answer['orderDetails']['orderTotalAmount'] / 100:.2f}"
+                    ),
+                    "billing_address": {
+                        "address": billing_details["address"],
+                        "city": billing_details["city"],
+                        "country": billing_details["country"],
+                        "first_name": billing_details["firstName"],
+                        "last_name": billing_details["lastName"],
+                        "postcode": billing_details["zipCode"],
+                    },
+                },
+            )
+        else:
+            self._do_on_payment_failure(order)
