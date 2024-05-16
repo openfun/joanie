e4974a76fd3d5504650393a9661ecb5d653913af
Nicolas Clerc <kernicpanel@nclerc.fr>
2024-05-16 14:06:41 +0200

    ✨(backend) add order states and flow for the new sales tunnel
    New order states are needed for the new sales tunnel:
    - ORDER_STATE_ASSIGNED
    - ORDER_STATE_TO_SAVE_PAYMENT_METHOD
    - ORDER_STATE_TO_SIGN
    - ORDER_STATE_TO_SIGN_AND_TO_SAVE_PAYMENT_METHOD

--- src/backend/joanie/core/enums.py
+++ src/backend/joanie/core/enums.py
@@ -58,6 +58,14 @@ CATALOG_VISIBILITY_CHOICES = (
 )
 
 ORDER_STATE_DRAFT = "draft"  # order has been created
+ORDER_STATE_ASSIGNED = "assigned"  # order has been assigned to an organization
+ORDER_STATE_TO_SAVE_PAYMENT_METHOD = (
+    "to_save_payment_method"  # order needs a payment method
+)
+ORDER_STATE_TO_SIGN = "to_sign"  # order needs a contract signature
+ORDER_STATE_TO_SIGN_AND_TO_SAVE_PAYMENT_METHOD = (
+    "to_sign_and_to_save_payment_method"  # order needs a contract signature and a payment method
+)  # fmt: skip
 ORDER_STATE_SUBMITTED = "submitted"  # order information have been validated
 ORDER_STATE_PENDING = "pending"  # payment has failed but can be retried
 ORDER_STATE_CANCELED = "canceled"  # has been canceled
@@ -69,6 +77,13 @@ ORDER_STATE_COMPLETED = "completed"  # is completed
 
 ORDER_STATE_CHOICES = (
     (ORDER_STATE_DRAFT, _("Draft")),  # default
+    (ORDER_STATE_ASSIGNED, _("Assigned")),
+    (ORDER_STATE_TO_SAVE_PAYMENT_METHOD, _("To save payment method")),
+    (ORDER_STATE_TO_SIGN, _("To sign")),
+    (
+        ORDER_STATE_TO_SIGN_AND_TO_SAVE_PAYMENT_METHOD,
+        _("To sign and to save payment method"),
+    ),
     (ORDER_STATE_SUBMITTED, _("Submitted")),
     (ORDER_STATE_PENDING, _("Pending")),
     (ORDER_STATE_CANCELED, pgettext_lazy("As in: the order is cancelled.", "Canceled")),
