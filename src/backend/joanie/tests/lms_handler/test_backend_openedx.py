ae88ab5713d35ccea122197319cf5a8e42bd4d91
Nicolas Clerc <kernicpanel@nclerc.fr>
2024-05-22 19:35:45 +0200

    ♻️(backend) add ProductTargetCourseRelation on order creation
    As order submit endpoint will be removed, we set ProductTargetCourseRelation
    directly on order creation.

--- src/backend/joanie/tests/lms_handler/test_backend_openedx.py
+++ src/backend/joanie/tests/lms_handler/test_backend_openedx.py
@@ -278,6 +278,7 @@ class OpenEdXLMSBackendTestCase(TestCase):
         order = factories.OrderFactory(product=product, owner=user)
         self.assertEqual(len(responses.calls), 0)
 
+        order.flow.assign()
         order.submit()
 
         self.assertEqual(len(responses.calls), 2)
