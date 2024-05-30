7e0876bb53439dd6b61cd21d9531fb87de660f72
Nicolas Clerc <kernicpanel@nclerc.fr>
2024-05-30 11:59:33 +0200

    💬(backend) fix order cancel error message
    As states changed, an error message needed to be updated accordingly.

--- src/backend/joanie/core/api/client/__init__.py
+++ src/backend/joanie/core/api/client/__init__.py
@@ -434,7 +434,7 @@ class OrderViewSet(
 
         if order.state == enums.ORDER_STATE_COMPLETED:
             return Response(
-                "Cannot cancel a validated order.",
+                "Cannot cancel a completed order.",
                 status=HTTPStatus.UNPROCESSABLE_ENTITY,
             )
 
