5ec65f90f9c97a673f15b4b5c14eb34c1542be43
Jonathan Reveille <jon.rev93001@gmail.com>
2024-06-10 19:03:50 +0200

    🎨(backend) update context for contract for terms and conditions
    The terms and conditions will be written and stored in the
    contract definition body field. There's no need to prepare them
    separately for the contract context anymore. We've updated the
    generation of context by removing the key `terms_and_conditions`
    in the preparation. In this commit, we have also updated
    the contract definition html template for the section "Appendices".
    The use of `SiteConfig` to prepare terms and conditions in html
    is now deprecated.

--- src/backend/joanie/tests/core/test_models_site_config.py
+++ src/backend/joanie/tests/core/test_models_site_config.py
@@ -19,50 +19,12 @@ class SiteConfigModelTestSuite(TestCase):
         It should return the terms and conditions in html format in the current language
         if it exists.
         """
-        terms_markdown_english = """
-        ## Terms and conditions
-        Here are the terms and conditions of the current site.
-        """
-
-        terms_markdown_french = """
-        ## Conditions générales de ventes
-        Voici les conditions générales de ventes du site.
-        """
-
         site_config = SiteConfigFactory()
 
-        # If no translation exists, it should return an empty string
-        self.assertEqual(site_config.get_terms_and_conditions_in_html(), "")
+        with self.assertRaises(DeprecationWarning) as deprecation_warning:
+            site_config.get_terms_and_conditions_in_html()
 
-        # Create default language and french translations of the terms and conditions
-        site_config.translations.create(terms_and_conditions=terms_markdown_english)
-        site_config.translations.create(
-            language_code="fr", terms_and_conditions=terms_markdown_french
-        )
-
-        # It should use the default language if no language is provided
         self.assertEqual(
-            site_config.get_terms_and_conditions_in_html(),
-            (
-                "<h2>Terms and conditions</h2>\n"
-                "<p>Here are the terms and conditions of the current site.</p>"
-            ),
-        )
-
-        # It should use the provided language if it exists
-        self.assertEqual(
-            site_config.get_terms_and_conditions_in_html("fr"),
-            (
-                "<h2>Conditions générales de ventes</h2>\n"
-                "<p>Voici les conditions générales de ventes du site.</p>"
-            ),
-        )
-
-        # It should fallback to the default language if the provided language does not exist
-        self.assertEqual(
-            site_config.get_terms_and_conditions_in_html("de"),
-            (
-                "<h2>Terms and conditions</h2>\n"
-                "<p>Here are the terms and conditions of the current site.</p>"
-            ),
+            str(deprecation_warning.exception),
+            "Terms and conditions are managed through contract definition body.",
         )
