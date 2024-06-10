4b4dd91500866d76f8c9760a08754ad86651627a
Jonathan Reveille <jon.rev93001@gmail.com>
2024-06-10 19:03:50 +0200

    🎨(backend) update context for contract for terms and conditions
    The terms and conditions will be written and stored in the
    contract definition body field. There's no need to prepare them
    separately for the contract context anymore. We've updated the
    generation of context by removing the key `terms_and_conditions`
    in the preparation. In this commit, we have also updated
    the contract definition html template for the section "Appendices".
    The use of `SiteConfig` to prepare terms and conditions in html
    is now deprecated.

--- src/backend/joanie/core/models/site.py
+++ src/backend/joanie/core/models/site.py
@@ -1,12 +1,9 @@
 """Site extension models for the Joanie project."""
 
-import textwrap
-
 from django.contrib.sites.models import Site
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-import markdown
 from parler import models as parler_models
 
 from joanie.core.models.base import BaseModel
@@ -40,11 +37,6 @@ class SiteConfig(parler_models.TranslatableModel, BaseModel):
 
     def get_terms_and_conditions_in_html(self, language=None):
         """Return the terms and conditions in html format."""
-        content = self.safe_translation_getter(
-            "terms_and_conditions",
-            language_code=language,
-            any_language=True,
-            default="",
+        raise DeprecationWarning(
+            "Terms and conditions are managed through contract definition body."
         )
-
-        return markdown.markdown(textwrap.dedent(content))
