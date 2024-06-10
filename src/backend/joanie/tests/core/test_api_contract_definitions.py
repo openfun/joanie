f94a55cdc5ad1c60c3b85841e6805946c73dfca0
Jonathan Reveille <jon.rev93001@gmail.com>
2024-06-10 19:03:50 +0200

    🎨(backend) update context for contract for terms and conditions
    The terms and conditions will be written and stored in the
    contract definition body field. There's no need to prepare them
    separately for the contract context anymore. We've updated the
    generation of context by removing the key `terms_and_conditions`
    in the preparation. In this commit, we have also updated
    the contract definition html template for the section "Appendices".
    The use of `SiteConfig` to prepare terms and conditions in html
    is now deprecated.

--- src/backend/joanie/tests/core/test_api_contract_definitions.py
+++ src/backend/joanie/tests/core/test_api_contract_definitions.py
@@ -5,8 +5,6 @@ Test suite for Contract Definition API.
 from http import HTTPStatus
 from io import BytesIO
 
-from django.contrib.sites.models import Site
-
 from pdfminer.high_level import extract_text as pdf_extract_text
 
 from joanie.core import factories
@@ -137,15 +135,13 @@ class ContractDefinitionApiTest(BaseAPITestCase):
         user = factories.UserFactory(
             email="student_do@example.fr", first_name="John Doe", last_name=""
         )
-        factories.SiteConfigFactory(
-            site=Site.objects.get_current(),
-            terms_and_conditions="""
+        factories.UserAddressFactory(owner=user)
+        contract_definition = factories.ContractDefinitionFactory(
+            body="""
             ## Terms and conditions
             Here are the terms and conditions of the current contract
             """,
         )
-        factories.UserAddressFactory(owner=user)
-        contract_definition = factories.ContractDefinitionFactory()
         token = self.get_user_token(user.username)
 
         response = self.client.get(
