40a74747f80fd48097219d882aa52518d2a3af34
Nicolas Clerc <kernicpanel@nclerc.fr>
2024-07-02 11:37:53 +0200

    🗃️(backend) store Order images through DocumentImage model
    Currently, we are storing order images (organization logo) into json
    field as base64 encoded string. This is weird as it takes a lot of space
    in database then we duplicate images in each certificate where they are used.
    
    That's why, we decide to stop that and store Order images in a DocumentImage.

--- src/backend/joanie/core/models/contracts.py
+++ src/backend/joanie/core/models/contracts.py
@@ -16,7 +16,7 @@ from django.utils.translation import gettext_lazy as _
 import markdown
 
 from joanie.core import enums
-from joanie.core.models.base import BaseModel
+from joanie.core.models.base import BaseModel, DocumentImage
 
 logger = logging.getLogger(__name__)
 
@@ -35,12 +35,18 @@ class ContractDefinition(BaseModel):
         verbose_name=_("language"),
         help_text=_("Language of the contract definition"),
     )
-
     name = models.CharField(
         _("template name"),
         max_length=255,
         choices=enums.CONTRACT_NAME_CHOICES,
         default=enums.CONTRACT_DEFINITION,
+    )
+    images = models.ManyToManyField(
+        to=DocumentImage,
+        verbose_name=_("images"),
+        related_name="contract_definitions",
+        editable=False,
+        blank=True,
     )
 
     class Meta:
