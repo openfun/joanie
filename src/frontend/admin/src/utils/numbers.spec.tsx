e1bf490d2d581b092ff97fc4ce9e5909ad14cc3f
jbpenrath <jb.penrath@gmail.com>
2024-08-22 23:41:54 +0200

    ✨(backoffice) add utils numbers.toDigitString
    Add an util to transform a number into a string. If the value is less than 10,
    it prefix it with a `0`

--- src/frontend/admin/src/utils/numbers.spec.tsx
+++ src/frontend/admin/src/utils/numbers.spec.tsx
@@ -1,17 +1,32 @@
-import { randomNumber } from "@/utils/numbers";
+import { randomNumber, toDigitString } from "@/utils/numbers";
 
-describe("<numbers />", () => {
-  it("get random number between 0 and 3", async () => {
-    let number = randomNumber(3);
-    expect(number).toBeLessThanOrEqual(3);
+describe("utils/numbers", () => {
+  describe("randomNumber", () => {
+    it("get random number between 0 and 3", () => {
+      let number = randomNumber(3);
+      expect(number).toBeLessThanOrEqual(3);
 
-    number = randomNumber(3);
-    expect(number).toBeLessThanOrEqual(3);
+      number = randomNumber(3);
+      expect(number).toBeLessThanOrEqual(3);
 
-    number = randomNumber(3);
-    expect(number).toBeLessThanOrEqual(3);
+      number = randomNumber(3);
+      expect(number).toBeLessThanOrEqual(3);
 
-    number = randomNumber(3);
-    expect(number).toBeLessThanOrEqual(3);
+      number = randomNumber(3);
+      expect(number).toBeLessThanOrEqual(3);
+    });
+  });
+
+  describe("toDigitString", () => {
+    it("converts number to digit string", () => {
+      let digit = toDigitString(9);
+      expect(digit).toBe("09");
+
+      digit = toDigitString(10);
+      expect(digit).toBe("10");
+
+      digit = toDigitString(1_000_001);
+      expect(digit).toBe("1000001");
+    });
   });
 });
--- src/frontend/admin/src/utils/numbers.ts
+++ src/frontend/admin/src/utils/numbers.ts
@@ -1,3 +1,11 @@
 export const randomNumber = (max: number): number => {
   return Math.floor(Math.random() * max) + 1;
 };
+
+export const toDigitString = (value: number) => {
+  if (value >= 10) {
+    return value.toString();
+  }
+
+  return `0${value}`;
+};
