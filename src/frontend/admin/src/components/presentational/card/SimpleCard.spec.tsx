9afddefa5aff9d8bba69c0980d86fe1751d67934
jbpenrath <jb.penrath@gmail.com>
2024-11-20 15:32:19 +0100

    💄(backoffice) fix dark mode issue
    When enabling dark mode, we forgot to update two components
     so those ones does not display well in dark.

--- src/frontend/admin/src/components/presentational/card/SimpleCard.spec.tsx
+++ src/frontend/admin/src/components/presentational/card/SimpleCard.spec.tsx
@@ -9,11 +9,6 @@ describe("<SimpleCard />", () => {
       </SimpleCard>,
     );
 
-    const simpleCard = await screen.findByTestId("simpleCard");
-    expect(simpleCard).toHaveStyle(
-      "box-shadow: rgb(145 158 171 / 20%) 0px 0px 2px 0px,rgb(145 158 171 / 12%) 0px 12px 24px -4px",
-    );
-
     screen.getByText("Hello !");
   });
 });
