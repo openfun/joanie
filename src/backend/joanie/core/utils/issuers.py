900e9969918395fda038db2124f4ea1050d0a5a0
jbpenrath <jb.penrath@gmail.com>
2025-01-06 10:34:18 +0100

    🔧(backend) set base_url to allow Weasyprint to find static and medias
    Currently, we bind our static and media files into our templates by using base64
     encoded strings. But we have to develop a new certificate template and this one
      contains numerous images and other statics and if we encode all those files in
       base64 the generation fails. So we digging into this issue, I identified we
       could use the `base_url` argument to set the root of the url to allow
       weasyprint to load properly statics and media during generation.
       Unfortunately I did not find a way by using an absolute or relative path...

--- src/backend/joanie/core/utils/issuers.py
+++ src/backend/joanie/core/utils/issuers.py
@@ -20,6 +20,10 @@ def generate_document(name: str, context: dict) -> bytes:
         Make sure that those files exist in the 'templates' folder.
         - If the context is equal to 'None' or is an empty '{}', it
         will render the document without the context data.
+
+    We use localhost:8000 as the base URL to allow WeasyPrint to find static/media files.
+    Running app through django dev server or gunicorn will run on the port 8000 so this
+    strategy works in development and production.
     """
     html_template_path = Path(f"issuers/{name}.html")
     css_template_name = Path(f"issuers/{name}.css")
@@ -27,11 +31,13 @@ def generate_document(name: str, context: dict) -> bytes:
     doc_html = HTML(
         string=Engine.get_default()
         .get_template(html_template_path)
-        .render(Context(context))
+        .render(Context(context)),
+        base_url="http://localhost:8000",
     )
     font_config = FontConfiguration()
     css = CSS(
         string=get_template(css_template_name).render(context),
         font_config=font_config,
+        base_url="http://localhost:8000",
     )
     return doc_html.write_pdf(stylesheets=[css], zoom=1, font_config=font_config)
