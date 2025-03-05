3f3a54bfe7df6e3b9ed2490a7802600f9e7d9b37
Nicolas Clerc <kernicpanel@nclerc.fr>
2025-03-05 17:58:14 +0100

    ✨(backend) add voucher model
    We want to allow users to use a voucher code to get dicounts on orders.

--- src/backend/joanie/tests/core/test_api_admin_order_group.py
+++ src/backend/joanie/tests/core/test_api_admin_order_group.py
@@ -344,7 +344,7 @@ class OrderGroupAdminApiTest(TestCase):
 
         relation = factories.CourseProductRelationFactory()
         order_group = factories.OrderGroupFactory(course_product_relation=relation)
-        with self.assertNumQueries(5):
+        with self.assertNumQueries(6):
             response = self.client.delete(
                 f"{self.base_url}/{relation.id}/order-groups/{order_group.id}/",
             )
@@ -360,7 +360,7 @@ class OrderGroupAdminApiTest(TestCase):
 
         relation = factories.CourseProductRelationFactory()
         order_group = factories.OrderGroupFactory(course_product_relation=relation)
-        with self.assertNumQueries(5):
+        with self.assertNumQueries(6):
             response = self.client.delete(
                 f"{self.base_url}/{relation.id}/order-groups/{order_group.id}/",
             )
