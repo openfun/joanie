7bfe2f1e8d163849038ee0ac08e00a7a2e05e8d8
Jonathan Reveille <jon.rev93001@gmail.com>
2025-07-23 15:05:20 +0200

    ✨(backend) add quote to admin batch order serializer
    Now that quote can be related to a batch order, it was necessary
    to have its detail into the batch order admin api viewset list
    and detail.

--- src/backend/joanie/tests/core/api/admin/batch_order/test_read_detail.py
+++ src/backend/joanie/tests/core/api/admin/batch_order/test_read_detail.py
@@ -72,5 +72,53 @@ class BatchOrdersAdminApiDetailTestCase(TestCase):
                 "vouchers": [],
                 "offering_rules": [],
                 "voucher": None,
+                "quote": None,
+            },
+        )
+
+    def test_api_admin_read_detail_batch_order_with_quote(self):
+        """
+        Admin user should be able to get the detail of a batch order with a quote
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+
+        batch_order = factories.BatchOrderFactory(state=enums.BATCH_ORDER_STATE_QUOTED)
+
+        response = self.client.get(f"/api/v1.0/admin/batch-orders/{batch_order.id}/")
+
+        self.assertEqual(response.status_code, HTTPStatus.OK, response.json())
+        self.assertDictEqual(
+            response.json(),
+            {
+                "id": str(batch_order.id),
+                "address": batch_order.address,
+                "city": batch_order.city,
+                "company_name": batch_order.company_name,
+                "contract_id": str(batch_order.contract.id),
+                "country": batch_order.country.code,
+                "currency": settings.DEFAULT_CURRENCY,
+                "identification_number": batch_order.identification_number,
+                "main_invoice_reference": str(batch_order.main_invoice.reference),
+                "nb_seats": batch_order.nb_seats,
+                "organization": {
+                    "code": batch_order.organization.code,
+                    "id": str(batch_order.organization.id),
+                    "title": batch_order.organization.title,
+                },
+                "owner": str(batch_order.owner.id),
+                "postcode": batch_order.postcode,
+                "offering": str(batch_order.offering.id),
+                "total": float(batch_order.total),
+                "trainees": batch_order.trainees,
+                "vouchers": [],
+                "offering_rules": [],
+                "voucher": None,
+                "quote": {
+                    "definition_title": batch_order.quote.definition.title,
+                    "has_purchase_order": False,
+                    "id": str(batch_order.quote.id),
+                    "organization_signed_on": batch_order.quote.organization_signed_on,
+                },
             },
         )
