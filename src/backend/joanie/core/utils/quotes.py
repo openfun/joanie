32cf368628e2beb576de365b2a0bfa481fcd0949
Jonathan Reveille <jon.rev93001@gmail.com>
2025-08-25 17:05:16 +0200

    ✨(backend) quote template and document context preparation
    There are new fields about the buyer's company that needs
    to be added to the quote. The billing address, funding
    entity and the tax registration number were added to
    the template. Since we are updating the quote, we have
    decided to take away sections that are not used
    anymore for the document (price, 2nd signature field).

--- src/backend/joanie/core/utils/quotes.py
+++ src/backend/joanie/core/utils/quotes.py
@@ -29,11 +29,28 @@ COURSE_FALLBACK_DATA = {
 CUSTOMER_FALLBACK_DATA = {
     "representative_name": _("<CUSTOMER_REPRESENTATIVE_NAME>"),
     "identification_number": _("<CUSTOMER_IDENTIFICATION_NUMBER>"),
+    "vat_registration": _("<CUSTOMER_VAT_REGISTRATION>"),
     "company_name": _("<CUSTOMER_COMPANY_NAME>"),
     "address": _("<CUSTOMER_ADDRESS>"),
     "postcode": _("<CUSTOMER_POSTCODE>"),
     "country": _("<CUSTOMER_COUNTRY>"),
     "city": _("<CUSTOMER_CITY>"),
+    "billing_address": {
+        "address": _("<BILLING_ADDRESS>"),
+        "postcode": _("<BILLING_POSTCODE>"),
+        "city": _("<BILLING_CITY>"),
+        "country": _("<BILLING_COUNTRY>"),
+        "company_name": _("<BILLING_COMPANY_NAME>"),
+        "contact_name": _("<BILLING_CONTACT_NAME>"),
+        "contact_email": _("<BILLING_CONTACT_EMAIL>"),
+    },
+    "administrative_firstname": _("<ADMIN_FIRSTNAME>"),
+    "administrative_lastname": _("<ADMIN_LASTNAME>"),
+    "administrative_profession": _("<ADMIN_PROFESSION>"),
+    "administrative_email": _("<ADMIN_EMAIL>"),
+    "administrative_telephone": _("<ADMIN_TELEPHONE>"),
+    "funding_entity": _("<FUNDING_ENTITY>"),
+    "funding_amount": _("<FUNDING_AMOUNT>"),
 }
 
 ORGANIZATION_FALLBACK_DATA = {
@@ -138,11 +155,28 @@ def prepare_customer_context(batch_order):
     return {
         "representative_name": batch_order.owner.name,
         "identification_number": batch_order.identification_number,
+        "vat_registration": batch_order.vat_registration,
         "company_name": batch_order.company_name,
         "address": batch_order.address,
         "postcode": batch_order.postcode,
         "country": str(batch_order.country.code),
         "city": batch_order.city,
+        "billing_address": {
+            "address": batch_order.billing_address["address"],
+            "postcode": batch_order.billing_address["postcode"],
+            "city": batch_order.billing_address["city"],
+            "country": batch_order.billing_address["country"],
+            "company_name": batch_order.billing_address["company_name"],
+            "contact_name": batch_order.billing_address["contact_name"],
+            "contact_email": batch_order.billing_address["contact_email"],
+        },
+        "administrative_firstname": batch_order.administrative_firstname,
+        "administrative_lastname": batch_order.administrative_lastname,
+        "administrative_profession": batch_order.administrative_profession,
+        "administrative_email": batch_order.administrative_email,
+        "administrative_telephone": batch_order.administrative_telephone,
+        "funding_entity": batch_order.funding_entity,
+        "funding_amount": batch_order.funding_amount,
     }
 
 
