d7f3c7971bddbad4a0ca424cd016e1edd15f2bc1
Jonathan Reveille <jon.rev93001@gmail.com>
2025-08-25 18:22:07 +0200

    ✨(backend) client api get organizations of an offering
    For the creation of a batch order, we want users to be able
    to select an organization that recommended them the training.
    We needed an endpoint that returns all organizations that can
    deliver the offering.

--- src/backend/joanie/core/api/client/__init__.py
+++ src/backend/joanie/core/api/client/__init__.py
@@ -206,7 +206,7 @@ class OfferingViewSet(
         """Return the serializer class to use."""
         if self.action in ["payment_schedule", "payment_plan"]:
             return serializers.OrderPaymentScheduleSerializer
-        if self.action == "list":
+        if self.action in ["list", "get_organizations"]:
             return serializers.OfferingLightSerializer
         return self.serializer_class
 
@@ -281,6 +281,17 @@ class OfferingViewSet(
             return str(voucher.discount)
 
         return offering.rules.get("discount", None)
+
+    @action(
+        methods=["GET"],
+        detail=True,
+        url_path="get-organizations",
+    )
+    def get_organizations(self, request, *args, **kwargs):
+        """Returns the list of organizations that delivers the offerings"""
+        offering = self.get_object()
+        serializer = self.get_serializer(offering)
+        return Response(serializer.data["organizations"], status=HTTPStatus.OK)
 
 
 class EnrollmentViewSet(
