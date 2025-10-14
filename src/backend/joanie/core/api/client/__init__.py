5548f49610ed79c877339d6d96db402afbd96e9c
Jonathan Reveille <jon.rev93001@gmail.com>
2025-10-14 16:43:04 +0200

    🧑‍💻(backend) add swagger parameter for confirm quote
    To improve the developers experience while using our
    swagger of the client API, we have added a missing parameter
    that is requested by the endpoint `confirm_quote`.

--- src/backend/joanie/core/api/client/__init__.py
+++ src/backend/joanie/core/api/client/__init__.py
@@ -1185,6 +1185,13 @@ class OrganizationViewSet(
                 type=OpenApiTypes.UUID,
                 many=False,
             ),
+            OpenApiParameter(
+                name="total",
+                description="Batch order total",
+                required=True,
+                type=OpenApiTypes.STR,
+                many=False,
+            ),
         ],
     )
     @action(
