da54e7bbc9ede7073d8c5eefd70e5e84bf2fb12a
Jonathan Reveille <jon.rev93001@gmail.com>
2025-10-20 10:04:53 +0200

    🐛(backend) validate voucher and course-product relation for order create
    We discovered that when a user uses the voucher code of a batch order
    with the wrong course-product relation it would still create the order
    on course-product related to the batch order. We added more control
    to prevent of this behavior. We want the payload to be accurate
    with the course-product relation that is related to the batch order
    and the order in `to_own` state.

--- src/backend/joanie/core/utils/order.py
+++ src/backend/joanie/core/utils/order.py
@@ -1,10 +1,11 @@
 """Util to manage the deletion of Order depending the state and the product type"""
 
-from joanie.core import enums
-from joanie.core.models import Order
+from uuid import UUID
+
+from joanie.core import enums, models
 
 
-def delete_stuck_signing_order(order):
+def delete_stuck_signing_order(order: models.Order):
     """
     Delete related objects and the Order itself when it is stuck in signing states.
     This method handles orders in the `ORDER_STATE_TO_SIGN` or `ORDER_STATE_SIGNING` states,
@@ -19,7 +20,7 @@ def delete_stuck_signing_order(order):
     order.delete()
 
 
-def delete_stuck_certificate_order(order):
+def delete_stuck_certificate_order(order: models.Order):
     """
     Delete related objects and the Order itself when it is stuck for a product type certificate
     in the state `ORDER_STATE_TO_SAVE_PAYMENT_METHOD`.
@@ -46,12 +47,45 @@ def delete_stuck_orders():
     deleted_orders_in_signing_states = 0
     deleted_orders_in_to_save_payment_state = 0
 
-    for order in Order.objects.get_stuck_signing_orders():
+    for order in models.Order.objects.get_stuck_signing_orders():
         delete_stuck_signing_order(order)
         deleted_orders_in_signing_states += 1
 
-    for order in Order.objects.get_stuck_certificate_payment_orders():
+    for order in models.Order.objects.get_stuck_certificate_payment_orders():
         delete_stuck_certificate_order(order)
         deleted_orders_in_to_save_payment_state += 1
 
     return deleted_orders_in_signing_states, deleted_orders_in_to_save_payment_state
+
+
+def verify_voucher(voucher_code: str) -> models.Voucher | None:
+    """
+    When a voucher code is used to create an order, we will verify if it exists and whether it's
+    active to be consumed. If the object is found, we return it, else we return None.
+    """
+    voucher = models.Voucher.objects.filter(
+        code__iexact=voucher_code, is_active=True
+    ).first()
+    if not voucher:
+        return None
+    return voucher
+
+
+def get_prepaid_order(
+    course_code: str, product_id: UUID, voucher_code: str
+) -> models.Order | None:
+    """
+    When an eligible voucher code is used to create an order, we will verify if it can be used
+    with the course and product relation passed. If we find the order in state `to_own` that
+    matches the information, we return it, else we return None.
+    """
+    try:
+        return models.Order.objects.get(
+            course__code=course_code,
+            product_id=product_id,
+            voucher__code=voucher_code,
+            voucher__discount__rate=1,
+            state=enums.ORDER_STATE_TO_OWN,
+        )
+    except models.Order.DoesNotExist:
+        return None
