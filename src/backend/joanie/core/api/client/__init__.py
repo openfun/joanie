e2c10fd2f60b660b0b473e7b9f5f09d30168e1cf
Jonathan Reveille <jon.rev93001@gmail.com>
2026-03-25 15:31:59 +0100

    🔥(backend) remove unnecessary batch order flow update
    We remove the call of batch order flow update into the endpoint
    of `confirm_purchase_order` endpoint in the `OrganizationViewSet`
    because it's already called within the method `tag_has_purchase_order`.
    Finally, it would call it twice. Once that is sufficient enough.

--- src/backend/joanie/core/api/client/__init__.py
+++ src/backend/joanie/core/api/client/__init__.py
@@ -1420,8 +1420,6 @@ class OrganizationViewSet(
             )
 
         quote.tag_has_purchase_order(purchase_order_reference=purchase_order_reference)
-        # Update the flow of batch order to sign
-        quote.batch_order.flow.update()
 
         return Response(status=HTTPStatus.OK)
 
