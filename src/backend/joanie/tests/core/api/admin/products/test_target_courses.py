3dc230ea5e8b7b9d24c310ed72e5c20b71a41a25
Jonathan Reveille <jon.rev93001@gmail.com>
2026-05-12 15:31:31 +0200

    ✨(backend) add session code and course code to export order csv
    Our API consumer needed the course code and the session code for
    the export CSV of orders. The sessions are written in the course run's
    title. To help developers, we added the necessary data to the command
    `dev-data` for each type of order nature and batch order types.

--- src/backend/joanie/tests/core/api/admin/products/test_target_courses.py
+++ src/backend/joanie/tests/core/api/admin/products/test_target_courses.py
@@ -4,7 +4,8 @@ Test suite for Product Admin API.
 
 from http import HTTPStatus
 
-from joanie.core import factories, models
+from joanie.core import enums, factories, models
+from joanie.core.models import CourseState
 from joanie.tests.base import BaseAPITestCase
 
 
@@ -217,3 +218,81 @@ class ProductAdminApiTargetCoursesTest(BaseAPITestCase):
         self.assertEqual(offerings.get(course=courses[0]).position, 2)
         self.assertEqual(offerings.get(course=courses[2]).position, 3)
         self.assertEqual(offerings.get(course=courses[4]).position, 4)
+
+    def test_admin_api_product_edit_target_courses_course_run_select_one(self):
+        """
+        An authenticated admin user should be able to update the course run
+        of the target courses of a product. When many course runs are available,
+        he should be able to select a specific course run to be active.
+        """
+        admin = factories.UserFactory(is_staff=True, is_superuser=True)
+        self.client.login(username=admin.username, password="password")
+
+        course = factories.CourseFactory(
+            code="00002",
+            title="Course for credential",
+        )
+        # Create a course run opened
+        factories.CourseRunFactory(
+            title="Course run credential - 00002",
+            course=course,
+            state=CourseState.ONGOING_OPEN,
+            is_listed=False,
+        )
+        # This course run should be the selected one for the order and in the target course
+        # of the product
+        course_run_credential_ongoing = factories.CourseRunFactory(
+            title="Course run credential - 00002-2",
+            course=course,
+            state=CourseState.ONGOING_OPEN,
+            is_listed=True,
+        )
+        # Create another course run opened
+        factories.CourseRunFactory(
+            title="Course run credential - 00002-3",
+            course=course,
+            state=CourseState.ONGOING_OPEN,
+            is_listed=False,
+        )
+        credential_product = factories.ProductFactory(
+            contract_definition_batch_order=factories.ContractDefinitionFactory(
+                name=enums.PROFESSIONAL_TRAINING_AGREEMENT_UNICAMP
+            ),
+            contract_definition_order=factories.ContractDefinitionFactory(
+                name=enums.CONTRACT_DEFINITION_UNICAMP
+            ),
+            quote_definition=factories.QuoteDefinitionFactory(),
+            type=enums.PRODUCT_TYPE_CREDENTIAL,
+            title="Credential Product",
+            courses=[course_run_credential_ongoing.course],
+            target_courses=[
+                course_run_credential_ongoing.course,
+            ],
+            price=100,
+        )
+        credential_offering = models.CourseProductRelation.objects.get(
+            course=credential_product.courses.first(),
+            product=credential_product,
+        )
+
+        # Update the active course run to the product target courses
+        self.client.patch(
+            f"/api/v1.0/admin/products/{credential_offering.product.id}"
+            f"/target-courses/{credential_offering.course.id}/",
+            data={
+                "course_runs": [
+                    f"{course_run_credential_ongoing.id}",
+                ]
+            },
+            content_type="application/json",
+        )
+        # Create an order that should target the selected course run of the target
+        # course of product
+        order = factories.OrderGeneratorFactory(
+            state=enums.ORDER_STATE_COMPLETED,
+            course=credential_offering.course,
+            product=credential_offering.product,
+        )
+
+        self.assertEqual(credential_product.target_course_runs.count(), 1)
+        self.assertEqual(order.target_course_runs.count(), 1)
